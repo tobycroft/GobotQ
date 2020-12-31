17b1d736106223f81ab0ace890c2203a796c756d
Tuuz <miracle-plus@outlook.com>
2020-12-31 14:18:14 +0800

    Tuuz

---
+++ app/bot/event/PrivateMsg.go
@@ -0,0 +1,39 @@
+package event
+
+type PM struct {
+	Type   string `json:"Type"`
+	FromQQ struct {
+		UIN      int    `json:"UIN"`
+		NickName string `json:"NickName"`
+	} `json:"FromQQ"`
+	LogonQQ   int `json:"LogonQQ"`
+	TimeStamp struct {
+		Recv int `json:"Recv"`
+		Send int `json:"Send"`
+	} `json:"TimeStamp"`
+	FromGroup struct {
+		GIN int `json:"GIN"`
+	} `json:"FromGroup"`
+	Msg struct {
+		Req         int    `json:"Req"`
+		Seq         int64  `json:"Seq"`
+		Type        int    `json:"Type"`
+		SubType     int    `json:"SubType"`
+		SubTempType int    `json:"SubTempType"`
+		Text        string `json:"Text"`
+		BubbleID    int    `json:"BubbleID"`
+	} `json:"Msg"`
+	Hb struct {
+		Type int `json:"Type"`
+	} `json:"Hb"`
+	File struct {
+		ID   string `json:"ID"`
+		MD5  string `json:"MD5"`
+		Name string `json:"Name"`
+		Size int    `json:"Size"`
+	} `json:"File"`
+}
+
+func PrivateMsg(pm PM) {
+
+}

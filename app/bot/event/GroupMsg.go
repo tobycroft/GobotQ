a5e41b55801c6ff3a7edb04f3bc9ee0b090de381
Tuuz <miracle-plus@outlook.com>
2020-12-31 15:51:27 +0800

    Tuuz

--- app/bot/event/GroupMsg.go
+++ app/bot/event/GroupMsg.go
@@ -1,5 +1,7 @@
 package event
 
+import "main.go/app/bot/model/GroupMsgModel"
+
 type GM struct {
 	Type   string `json:"Type"`
 	FromQQ struct {
@@ -38,5 +40,6 @@ type GM struct {
 }
 
 func GroupMsg(gm GM) {
-
+	GroupMsgModel.Api_insert(gm.LogonQQ, gm.FromQQ.UIN, gm.FromGroup.GIN, gm.Msg.Text, gm.Msg.Req, gm.Msg.Random, gm.File.ID, gm.File.MD5,
+		gm.File.Name, gm.File.Size)
 }

d0fe3996bcfe5cab7cee8853e923e2b620f379eb
Tuuz <miracle-plus@outlook.com>
2020-12-31 14:05:24 +0800

    Tuuz

---
+++ app/bot/model/LogRecvModel/LogsModel.go
@@ -0,0 +1,23 @@
+package LogRecvModel
+
+import (
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "log_recv"
+
+func Api_insert(log interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"log": log,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}

5ac64afd7ce710e9c85fec094cdf7986756693e1
Tuuz <miracle-plus@outlook.com>
2020-12-31 14:50:13 +0800

    Tuuz

--- app/bot/event/event.go
+++ app/bot/event/event.go
@@ -1,6 +1,7 @@
 package event
 
 import (
+	jsoniter "github.com/json-iterator/go"
 	"main.go/app/bot/model/LogErrorModel"
 	"main.go/app/bot/model/LogRecvModel"
 	"main.go/app/bot/model/LogsModel"
@@ -11,10 +12,25 @@ import (
 func EventRouter(json string) {
 	//save the data in the first place
 	LogsModel.Api_insert(json, "main")
-	_, err := Jsong.JObject(json)
+	data, err := Jsong.JObject(json)
 	if err != nil {
 		LogErrorModel.Api_insert(err.Error(), tuuz.FUNCTION_ALL())
 	} else {
+		Type := data["type"]
+		if Type == nil {
+			return
+		}
+		switch Type {
+		case "PrivateMsg":
+			jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+			var pm PM
+			jsr.UnmarshalFromString(json, &pm)
+			PrivateMsg(pm)
+			break
+
+		default:
+			break
+		}
 		LogRecvModel.Api_insert(json)
 	}
 }

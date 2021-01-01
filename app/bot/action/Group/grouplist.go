05a8427ed8a9ec3df8e557d8f069768d6eac487f
Tuuz <silverhawk@foxmail.com>
2021-01-02 01:48:42 +0800

    TuuzPC

--- app/bot/action/Group/grouplist.go
+++ app/bot/action/Group/grouplist.go
@@ -3,28 +3,29 @@ package Private
 import (
 	"main.go/app/bot/api"
 	"main.go/app/bot/model/BotModel"
-	"main.go/app/bot/model/FriendListModel"
+	"main.go/app/bot/model/GroupListModel"
 )
 
 func App_fresh_group_list() {
 	bots := BotModel.Api_select()
 	for _, bot := range bots {
-		fl, err := api.Getgrouplist(bot["bot"])
+		gl, err := api.Getgrouplist(bot["bot"])
 		if err != nil {
 
 		} else {
-			FriendListModel.Api_delete(bot["bot"])
-			var fss []FriendListModel.FriendList
-			for _, fll := range fl {
-				var fs FriendListModel.FriendList
-				fs.Bot = bot["bot"]
-				fs.Uid = fll.UIN
-				fs.Nickname = fll.NickName
-				fs.Email = fll.Email
-				fs.Remark = fll.Remark
-				fss = append(fss, fs)
+			GroupListModel.Api_delete(bot["bot"])
+			var gss []GroupListModel.GroupList
+			for _, gll := range gl {
+				var gs GroupListModel.GroupList
+				gs.Bot = bot["bot"]
+				gs.Gid = gll.GIN
+				gs.Group_name = gll.StrGroupName
+				gs.Group_memo = gll.StrGroupMemo
+				gs.Owner = gll.DwGroupOwnerUin
+				gs.Number = gll.DwMemberNum
+				gss = append(gss, gs)
 			}
-			FriendListModel.Api_insert_more(fss)
+			GroupListModel.Api_insert_more(gss)
 		}
 	}
 }

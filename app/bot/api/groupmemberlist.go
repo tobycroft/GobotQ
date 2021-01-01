49a1ebf453c1d2711b4b26516d691da99daf93d6
Tuuz <silverhawk@foxmail.com>
2021-01-02 01:56:13 +0800

    TuuzPC

---
+++ app/bot/api/groupmemberlist.go
@@ -0,0 +1,49 @@
+package api
+
+import (
+	"fmt"
+	jsoniter "github.com/json-iterator/go"
+	"main.go/config/app_conf"
+	"main.go/tuuz/Net"
+)
+
+type Gms struct {
+	Ret  string          `json:"ret"`
+	List GroupMemberList `json:"List"`
+}
+
+type GroupMemberList []struct {
+	UIN              int    `json:"UIN"`
+	Age              int    `json:"Age"`
+	Sex              int    `json:"Sex"`
+	NickName         string `json:"NickName"`
+	Email            string `json:"Email"`
+	Card             string `json:"Card"`
+	Remark           string `json:"Remark"`
+	SpecTitle        string `json:"SpecTitle"`
+	Phone            string `json:"Phone"`
+	SpecTitleExpired int    `json:"SpecTitleExpired"`
+	MuteTime         int    `json:"MuteTime"`
+	AddGroupTime     int    `json:"AddGroupTime"`
+	LastMsgTime      int    `json:"LastMsgTime"`
+	GroupLevel       int    `json:"GroupLevel"`
+}
+
+func Getgroupmemberlist(bot, group interface{}) (GroupMemberList, error) {
+	post := map[string]interface{}{
+		"logonqq": bot,
+		"group":   group,
+	}
+	data, err := Net.Post(app_conf.Http_Api+"/getgroupmemberlist", nil, post, nil, nil)
+	fmt.Println(data)
+	if err != nil {
+		return nil, err
+	}
+	var gms Gms
+	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	err = jsr.UnmarshalFromString(data, &gms)
+	if err != nil {
+		return nil, err
+	}
+	return gms.List, nil
+}

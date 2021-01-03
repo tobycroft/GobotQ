0cd141a7e5e0f763a11ad678d94e2a03abd659a8
Tuuz <silverhawk@foxmail.com>
2021-01-04 00:06:57 +0800

    TuuzPC

--- app/bot/service/at.go
+++ app/bot/service/at.go
@@ -2,8 +2,8 @@ package service
 
 import "main.go/tuuz/Calc"
 
-func Serv_at(qq int) string {
-	return "[@" + Calc.Int2String(qq) + "]"
+func Serv_at(qq interface{}) string {
+	return "[@" + Calc.Any2String(qq) + "]"
 }
 
 func Serv_at_all() string {

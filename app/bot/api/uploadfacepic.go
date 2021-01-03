d6ca7b1b89b5cdd615a8f2c5e99b5d12032cf79b
Tuuz <silverhawk@foxmail.com>
2021-01-03 20:35:18 +0800

    TuuzPC

---
+++ app/bot/api/uploadfacepic.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"errors"
+	jsoniter "github.com/json-iterator/go"
+	"main.go/config/app_conf"
+	"main.go/tuuz/Net"
+)
+
+type UploadFacePic struct {
+	Ret string `json:"ret"`
+}
+
+type UploadFacePicRet struct {
+	Retcode int    `json:"retcode"`
+	Retmsg  string `json:"retmsg"`
+	Time    string `json:"time"`
+}
+
+func Uploadfacepic(fromqq, togroup, text interface{}) (UploadFacePic, UploadFacePicRet, error) {
+	post := map[string]interface{}{
+		"fromqq":  fromqq,
+		"togroup": togroup,
+		"text":    text,
+	}
+	data, err := Net.Post(app_conf.Http_Api+"/UploadFacePic", nil, post, nil, nil)
+	if err != nil {
+		return UploadFacePic{}, UploadFacePicRet{}, err
+	}
+	var ret1 UploadFacePic
+	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	err = jsr.UnmarshalFromString(data, &ret1)
+	if err != nil {
+		return UploadFacePic{}, UploadFacePicRet{}, err
+	}
+	var ret2 UploadFacePicRet
+	err = jsr.UnmarshalFromString(ret1.Ret, &ret2)
+	if err != nil {
+		return ret1, UploadFacePicRet{}, err
+	}
+	if ret2.Retcode != 0 {
+		return ret1, ret2, errors.New(ret2.Retmsg)
+	}
+	return ret1, ret2, nil
+}

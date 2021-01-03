a69fb9367a86f3085b61ea0304854d88766d65bf
Tuuz <silverhawk@foxmail.com>
2021-01-03 20:15:35 +0800

    TuuzPC

--- app/bot/api/deleteprivatemsg.go
+++ app/bot/api/deleteprivatemsg.go
@@ -1,5 +1,37 @@
 package api
 
-func Deleteprivatemsg() {
+import (
+	"errors"
+	jsoniter "github.com/json-iterator/go"
+	"main.go/config/app_conf"
+	"main.go/tuuz/Net"
+)
 
+func Deleteprivatemsg(fromqq, toqq, random, req, time interface{}) {
+	post := map[string]interface{}{
+		"fromqq": fromqq,
+		"toqq":   toqq,
+		"random": random,
+		"req":    req,
+		"time":   time,
+	}
+	data, err := Net.Post(app_conf.Http_Api+"/sendprivatemsg", nil, post, nil, nil)
+	if err != nil {
+		return PrivateMsg{}, PrivateMsgRet{}, err
+	}
+	var pm PrivateMsg
+	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	err = jsr.UnmarshalFromString(data, &pm)
+	if err != nil {
+		return PrivateMsg{}, PrivateMsgRet{}, err
+	}
+	var ret PrivateMsgRet
+	err = jsr.UnmarshalFromString(pm.Ret, &ret)
+	if err != nil {
+		return pm, PrivateMsgRet{}, err
+	}
+	if ret.Retcode != 0 {
+		return pm, ret, errors.New(ret.Retmsg)
+	}
+	return pm, ret, nil
 }

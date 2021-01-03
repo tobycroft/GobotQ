0fa7d575d64a9a1085014acdb4dd4da38776aa78
Tuuz <silverhawk@foxmail.com>
2021-01-03 23:47:58 +0800

    TuuzPC

--- app/bot/action/Group/Sign.go
+++ app/bot/action/Group/Sign.go
@@ -1,14 +1,13 @@
 package Group
 
 import (
-	"main.go/app/bot/api"
 	"main.go/app/bot/model/GroupSignModel"
 )
 
 func App_group_sign(gid, uid interface{}) {
 	sign := GroupSignModel.Api_find(gid, uid)
 	if len(sign) > 0 {
-		api.Sendgroupmsg()
+
 	} else {
 
 	}

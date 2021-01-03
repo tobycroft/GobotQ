83d2db8ea1c60c516b07372b9618bc177f1ab012
Tuuz <silverhawk@foxmail.com>
2021-01-03 23:50:44 +0800

    TuuzPC

--- app/bot/action/Group/Sign.go
+++ app/bot/action/Group/Sign.go
@@ -1,6 +1,7 @@
 package Group
 
 import (
+	"main.go/app/bot/model/GroupBalanceModel"
 	"main.go/app/bot/model/GroupSignModel"
 )
 
@@ -9,6 +10,6 @@ func App_group_sign(gid, uid interface{}) {
 	if len(sign) > 0 {
 
 	} else {
-
+		group_model := GroupBalanceModel.Api_find(gid, uid)
 	}
 }

65197837165e4bc07f84d358fb44fdb01b95a5a4
Tuuz <silverhawk@foxmail.com>
2021-01-03 21:03:27 +0800

    TuuzPC

---
+++ app/bot/api/exitgroup.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	jsoniter "github.com/json-iterator/go"
+	"main.go/config/app_conf"
+	"main.go/tuuz/Net"
+)
+
+type ExitGroupRet struct {
+	Ret string `json:"ret"`
+}
+
+func Exitgroup(fromqq, group interface{}) (bool, error) {
+	post := map[string]interface{}{
+		"fromqq": fromqq,
+		"group":  group,
+	}
+	data, err := Net.Post(app_conf.Http_Api+"/setnickname", nil, post, nil, nil)
+	if err != nil {
+		return false, err
+	}
+	var ret SetNickNameRet
+	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	err = jsr.UnmarshalFromString(data, &ret)
+	if err != nil {
+		return false, err
+	}
+	if ret.Ret == "true" {
+		return true, nil
+	} else {
+		return false, nil
+	}
+}

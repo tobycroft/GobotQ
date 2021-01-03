9ada170f94065593a233e48de530a45218496d6e
Tuuz <silverhawk@foxmail.com>
2021-01-03 23:43:09 +0800

    TuuzPC

---
+++ app/bot/model/GroupBalanceModel/GroupBalanceModel.go
@@ -0,0 +1,104 @@
+package GroupBalanceModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/config/app_conf"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "group_balance"
+
+func Api_insert(gid, uid interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"gid": gid,
+		"uid": uid,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_select(gid interface{}) []gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"gid": gid,
+	}
+	db.Where(where)
+	db.Limit(app_conf.Db_default_load_limit)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_select_uid(uid interface{}) []gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"uid": uid,
+	}
+	db.Where(where)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_find(uid interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"uid": uid,
+	}
+	db.Where(where)
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_incr(gid, uid, balance_inc interface{}) bool {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"gid": gid,
+		"uid": uid,
+	}
+	db.Where(where)
+	_, err := db.Increment("balance", balance_inc)
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_decr(gid, uid, balance_decr interface{}) bool {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"gid": gid,
+		"uid": uid,
+	}
+	db.Where(where)
+	_, err := db.Decrement("balance", balance_decr)
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}

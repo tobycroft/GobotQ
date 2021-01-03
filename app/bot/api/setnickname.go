b8df3610e769cc949d689682b6523af96afe279d
Tuuz <silverhawk@foxmail.com>
2021-01-03 21:01:02 +0800

    TuuzPC

--- app/bot/api/setnickname.go
+++ app/bot/api/setnickname.go
@@ -10,20 +10,24 @@ type SetNickNameRet struct {
 	Ret string `json:"ret"`
 }
 
-func Setnickname(fromqq, nickname interface{}) (SetNickNameRet, error) {
+func Setnickname(fromqq, nickname interface{}) (bool, error) {
 	post := map[string]interface{}{
 		"fromqq":   fromqq,
 		"nickname": nickname,
 	}
 	data, err := Net.Post(app_conf.Http_Api+"/setnickname", nil, post, nil, nil)
 	if err != nil {
-		return SetNickNameRet{}, err
+		return false, err
 	}
-	var dpmr SetNickNameRet
+	var ret SetNickNameRet
 	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
-	err = jsr.UnmarshalFromString(data, &dpmr)
+	err = jsr.UnmarshalFromString(data, &ret)
 	if err != nil {
-		return SetNickNameRet{}, err
+		return false, err
 	}
-	return dpmr, nil
+	if ret.Ret == "true" {
+		return true, nil
+	} else {
+		return false, nil
+	}
 }

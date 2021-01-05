11b3a40b3ddc6d136ed22ee1bb8cb15913311fd7
Tuuz <silverhawk@foxmail.com>
2021-01-05 22:30:50 +0800

    TuuzPC

--- app/bot/action/Private/UserLogin.go
+++ app/bot/action/Private/UserLogin.go
@@ -1,5 +1,14 @@
 package Private
 
-func UserLogin(bot int, uid int, text string) {
+import (
+	"fmt"
+	"main.go/app/bot/api"
+	"main.go/tuuz/Calc"
+)
 
+func UserLogin(bot int, uid int, text string) {
+	rand := Calc.Rand(10000000, 99999999)
+	token := Calc.GenerateToken()
+	api.Sendprivatemsg(bot, uid, "您的登录密码\r\n"+Calc.Int2String(rand))
+	fmt.Println(token)
 }

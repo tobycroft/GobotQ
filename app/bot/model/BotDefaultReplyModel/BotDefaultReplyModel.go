68ba1e130891662911629bfc9c69df189e9f41bf
Tuuz <silverhawk@foxmail.com>
2021-01-05 22:24:54 +0800

    TuuzPC

---
+++ app/bot/model/BotDefaultReplyModel/BotDefaultReplyModel.go
@@ -0,0 +1,20 @@
+package PrivateAutoReplyModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "bot_default_reply"
+
+func Api_select(key interface{}) []gorose.Data {
+	db := tuuz.Db().Table(table)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}

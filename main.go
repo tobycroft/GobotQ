2c4def1db9f0df76787225b2cc18be249cac5685
Tuuz <miracle-plus@outlook.com>
2021-01-05 11:44:04 +0800

    Tuuz

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"main.go/app/bot/action/Group"
+	"main.go/app/bot/cron"
 	"main.go/route"
 )
 
 func main() {
 
 	go Group.App_refresh_group_member_chan()
+
+	go cron.Retract()
 
 	mainroute := gin.Default()
 	//gin.SetMode(gin.ReleaseMode)

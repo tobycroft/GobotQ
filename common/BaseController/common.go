5c68abd7d5e42e1934c877c84150f4b9e8b48d42
Tuuz <miracle-plus@outlook.com>
2021-01-06 16:33:29 +0800

    TuuzMac

--- common/BaseController/common.go
+++ common/BaseController/common.go
@@ -13,7 +13,6 @@ func CommonController() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Origin", "*")
 		c.Header("S-P-I", c.ClientIP())
 		c.Header("S-P-P", app_conf.Project)
-		c.Header("S-P-M", app_conf.AppMode)
 		//c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
 		//c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
 		//c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")

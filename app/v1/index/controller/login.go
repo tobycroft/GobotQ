212e673fe0e945214f15ad74ef6926981de44d90
Tuuz <miracle-plus@outlook.com>
2021-01-06 17:27:57 +0800

    TuuzMac

--- app/v1/index/controller/login.go
+++ app/v1/index/controller/login.go
@@ -2,9 +2,15 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/common/BaseController"
 )
 
 func LoginController(route *gin.RouterGroup) {
-	route.Use()
+	route.Use(BaseController.CommonController())
 
+	route.Any("login", login)
+}
+
+func login() {
+	qq
 }

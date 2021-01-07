8ca063bd0b7c51fdc4598b17c12bbfd3b845d0e2
Tuuz <silverhawk@foxmail.com>
2021-01-07 21:06:32 +0800

    TuuzPC

--- app/bot/action/Private/UserChangePasswordAction.go
+++ app/bot/action/Private/UserChangePasswordAction.go
@@ -3,10 +3,13 @@ package Private
 import (
 	"main.go/app/bot/api"
 	"main.go/app/bot/model/UserMemberModel"
+	"main.go/config/app_default"
 )
 
 func App_userChangePassword(bot int, uid int, text string) {
 	if UserMemberModel.Api_update_password(uid, text) {
 		api.Sendprivatemsg(bot, uid, "您的密码已被修改为：\r\n↓↓↓↓↓↓↓↓"+text+"↑↑↑↑↑↑↑↑")
+	} else {
+		api.Sendprivatemsg(bot, uid, app_default.Default_error_alert)
 	}
 }

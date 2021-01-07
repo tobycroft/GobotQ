fc850a45600631860a0e70b6df85bff968e01d10
Tuuz <silverhawk@foxmail.com>
2021-01-08 00:27:45 +0800

    TuuzPC

--- app/v1/user/controller/user.go
+++ app/v1/user/controller/user.go
@@ -16,6 +16,7 @@ func user_info(c *gin.Context) {
 	uid := c.PostForm("uid")
 	user := UserMemberModel.Api_find(uid)
 	if len(user) > 0 {
+		delete(user, "password")
 		RET.Success(c, 0, user, nil)
 	} else {
 		RET.Fail(c, 404, user, "没有找到数据")

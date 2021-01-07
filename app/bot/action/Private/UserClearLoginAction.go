2965d0a344d17123a797b637db74a4c67598a54c
Tuuz <silverhawk@foxmail.com>
2021-01-07 21:03:27 +0800

    TuuzPC

--- app/bot/action/Private/UserClearLoginAction.go
+++ app/bot/action/Private/UserClearLoginAction.go
@@ -1,5 +1,12 @@
 package Private
 
-func App_userClearLogin(bot int, uid int, text string) {
+import (
+	"main.go/app/bot/api"
+	"main.go/app/bot/model/UserTokenModel"
+)
 
+func App_userClearLogin(bot int, uid int, text string) {
+	if UserTokenModel.Api_delete(uid) {
+		api.Sendprivatemsg(bot, uid, "您的登录状态已经全部清空，如需再次登录请发送acfur登录")
+	}
 }

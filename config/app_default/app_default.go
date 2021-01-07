53320933103df887971caab3f64db8ea7d02be3f
Tuuz <silverhawk@foxmail.com>
2021-01-07 20:41:58 +0800

    TuuzPC

--- config/app_default/app_default.go
+++ config/app_default/app_default.go
@@ -3,4 +3,4 @@ package app_default
 const Default_private_help = "这里是帮助信息"
 
 const Default_str_login_text = "如果忘记密码或需要修改密码，再次发送登录即可\r\n如果需要自定义密码，" +
-	"请输入:acfur密码+要设定的密码,例如acfur密码123456\r\n密码长度允许1-16位,没有复杂度限制，可设定中文密码"
+	"请输入:\r\nacfur密码+要设定的密码\r\n例如acfur密码123456\r\n密码长度允许1-16位,没有复杂度限制，可设定中文密码"

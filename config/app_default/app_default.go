dfaea3b8118ff469fdee759905242a1c8a90a702
Tuuz <miracle-plus@outlook.com>
2021-01-07 11:14:11 +0800

    TuuzMac

--- config/app_default/app_default.go
+++ config/app_default/app_default.go
@@ -1,3 +1,5 @@
 package app_default
 
 const Default_private_help = "这里是帮助信息"
+
+const Default_str_login_text = "如果忘记密码或需要修改密码，再次发送登录即可\r\n如果需要自定义密码，请输入:acfur密码+要设定的密码,例如acfur密码123456\r\n密码没有限制，但是为了您的安全，我们建议您设定8位以上的密码"

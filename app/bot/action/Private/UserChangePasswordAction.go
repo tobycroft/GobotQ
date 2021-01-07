046b8c1cd2dc95319d913e9a94853c4f693fb40d
Tuuz <silverhawk@foxmail.com>
2021-01-07 21:08:41 +0800

    TuuzPC

--- app/bot/action/Private/UserChangePasswordAction.go
+++ app/bot/action/Private/UserChangePasswordAction.go
@@ -2,15 +2,23 @@ package Private
 
 import (
 	"main.go/app/bot/api"
+	"main.go/app/bot/model/LogErrorModel"
 	"main.go/app/bot/model/UserMemberModel"
 	"main.go/config/app_default"
 	"main.go/tuuz"
 )
 
 func App_userChangePassword(bot int, uid int, text string) {
+	if len(text) < 1 {
+		api.Sendprivatemsg(bot, uid, "密码长度应该大于1位")
+	}
+	if len(text) > 16 {
+		api.Sendprivatemsg(bot, uid, "密码长度应该小于等于16位")
+	}
 	if UserMemberModel.Api_update_password(uid, text) {
 		api.Sendprivatemsg(bot, uid, "您的密码已被修改为：\r\n↓↓↓↓↓↓↓↓"+text+"↑↑↑↑↑↑↑↑")
 	} else {
-		api.Sendprivatemsg(bot, uid, app_default.Default_error_alert+tuuz.FUNCTION_ALL())
+		LogErrorModel.Api_insert("修改密码错误", tuuz.FUNCTION_ALL())
+		api.Sendprivatemsg(bot, uid, app_default.Default_error_alert)
 	}
 }

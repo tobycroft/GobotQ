2e0dcb38b1e157dca0b4ca475ab3ed5244a69686
Tuuz <miracle-plus@outlook.com>
2021-01-07 15:47:46 +0800

    TuuzMac

---
+++ app/bot/api/getnickname.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	jsoniter "github.com/json-iterator/go"
+	"main.go/config/app_conf"
+	"main.go/tuuz/Net"
+)
+
+type GetNickNameRet struct {
+	Ret string `json:"ret"`
+}
+
+func Getnickname(fromqq, toqq, fromcache interface{}) (GetNickNameRet, error) {
+	post := map[string]interface{}{
+		"fromqq":    fromqq,
+		"toqq":      toqq,
+		"fromcache": fromcache,
+	}
+	data, err := Net.Post(app_conf.Http_Api+"/getnickname", nil, post, nil, nil)
+	if err != nil {
+		return GetNickNameRet{}, err
+	}
+	var ret LoginqqRet
+	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	err = jsr.UnmarshalFromString(data, &ret)
+	if err != nil {
+		return GetNickNameRet{}, err
+	}
+	return ret, nil
+}

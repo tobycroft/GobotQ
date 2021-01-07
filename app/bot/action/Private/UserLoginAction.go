6f5dc7f040516b6f8324574dd1a04490aac6016b
Tuuz <miracle-plus@outlook.com>
2021-01-07 15:50:13 +0800

    TuuzMac

--- app/bot/action/Private/UserLoginAction.go
+++ app/bot/action/Private/UserLoginAction.go
@@ -9,15 +9,21 @@ import (
 
 func App_UserLogin(bot int, uid int, text string) {
 	rand := Calc.Rand(10000000, 99999999)
-
+	uname := ""
+	nickname, err := api.Getnickname(bot, uid, true)
+	if err != nil {
+		uname = Calc.Int2String(uid)
+	} else {
+		uname = nickname.Ret
+	}
 	usermember := UserMemberModel.Api_find(uid)
 	if len(usermember) > 0 {
-		if !UserMemberModel.Api_update_password(uid, rand) {
+		if !UserMemberModel.Api_update_all(uid, uname, rand) {
 			api.Sendprivatemsg(bot, uid, "您的登录密码：\r\n"+Calc.Int2String(rand))
 			return
 		}
 	} else {
-		if !UserMemberModel.Api_insert(uid, uid, rand) {
+		if !UserMemberModel.Api_insert(uid, uname, rand) {
 			api.Sendprivatemsg(bot, uid, "↓↓↓↓↓您的登录密码↓↓↓↓↓\r\n"+Calc.Int2String(rand)+"\r\n↑↑↑↑↑请在APP中输入↑↑↑↑↑\r\n"+app_default.Default_str_login_text)
 		}
 	}

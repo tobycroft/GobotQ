98901ace35a74a94c2a41f76d1aa1c68129d37df
Tuuz <miracle-plus@outlook.com>
2021-01-08 12:46:17 +0800

    TuuzMac

--- app/v1/bot/controller/list.go
+++ app/v1/bot/controller/list.go
@@ -1,7 +1,16 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/common/BaseController"
+)
 
 func ListController(route *gin.RouterGroup) {
+	route.Use(BaseController.CommonController(), gin.Recovery())
+
+	route.Any("list_unbind", list_unbind)
+}
+
+func list_unbind(c *gin.Context) {
 
 }

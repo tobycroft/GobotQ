028b45a61e8a2952c8782a15489758cc90c787c5
Tuuz <miracle-plus@outlook.com>
2021-01-08 13:55:10 +0800

    TuuzMac

--- app/v1/bot/controller/list.go
+++ app/v1/bot/controller/list.go
@@ -2,15 +2,18 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/app/bot/model/BotModel"
 	"main.go/common/BaseController"
+	"main.go/tuuz/RET"
 )
 
 func ListController(route *gin.RouterGroup) {
-	route.Use(BaseController.CommonController(), gin.Recovery())
+	route.Use(BaseController.LoginedController(), gin.Recovery())
 
 	route.Any("list_unbind", list_unbind)
 }
 
 func list_unbind(c *gin.Context) {
-
+	unbinds := BotModel.Api_select_byOwner(0)
+	RET.Success(c, 0, unbinds, nil)
 }

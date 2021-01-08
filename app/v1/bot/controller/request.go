becf6580106bcac0e862b9911bd151035e9b318f
Tuuz <miracle-plus@outlook.com>
2021-01-08 14:10:02 +0800

    TuuzMac

--- app/v1/bot/controller/request.go
+++ app/v1/bot/controller/request.go
@@ -4,6 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func RequestController(route *gin.RouterGroup) {
 
+	route.Any("join", request_join)
+	route.Any("out", request_out)
 }
 
 func request_join(c *gin.Context) {

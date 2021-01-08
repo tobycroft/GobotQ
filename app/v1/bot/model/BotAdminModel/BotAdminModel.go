1bddfcaa089d2b7f0a597485a3f56d6ad45f14b2
Tuuz <miracle-plus@outlook.com>
2021-01-08 15:17:30 +0800

    TuuzMac

---
+++ app/v1/bot/model/BotAdminModel/BotAdminModel.go
@@ -0,0 +1,57 @@
+package BotAdminModel
+
+import (
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "bot_admin"
+
+func Api_insert(bot, qq, end_time interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"bot":      bot,
+		"qq":       qq,
+		"end_time": end_time,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_delete(bot, qq interface{}) bool {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"bot": bot,
+		"qq":  qq,
+	}
+	db.Where(where)
+	_, err := db.Delete()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_inc_endTime(bot, qq interface{}, endTime_incr int) bool {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"bot": bot,
+		"qq":  qq,
+	}
+	db.Where(where)
+	_, err := db.Increment("endTime_incr", endTime_incr)
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}

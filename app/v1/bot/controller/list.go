056347ce31e950ecccea53e5946a888bc5c1f039
Tuuz <miracle-plus@outlook.com>
2021-01-08 15:28:22 +0800

    TuuzMac

--- app/v1/bot/controller/list.go
+++ app/v1/bot/controller/list.go
@@ -3,13 +3,30 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"main.go/app/bot/model/BotModel"
+	"main.go/app/v1/bot/model/BotAdminModel"
 	"main.go/common/BaseController"
+	"main.go/tuuz/Input"
 	"main.go/tuuz/RET"
 )
 
 func ListController(route *gin.RouterGroup) {
 	route.Use(BaseController.LoginedController(), gin.Recovery())
-
+	route.Use(func(c *gin.Context) {
+		uid := c.PostForm("uid")
+		bot, ok := Input.PostInt("bot", c)
+		if !ok {
+			return
+		}
+		data := BotModel.Api_find_byOwnerandBot(uid, bot)
+		if len(data) > 0 {
+			c.Next()
+			return
+		} else {
+			RET.Fail(c, 403, nil, "你并不拥有这个机器人")
+			c.Abort()
+			return
+		}
+	})
 	route.Any("unbind", list_unbind)
 	route.Any("owned", list_yours_own)
 }
@@ -27,5 +44,6 @@ func list_yours_own(c *gin.Context) {
 
 func list_your_control(c *gin.Context) {
 	uid := c.PostForm("uid")
-
+	bot := c.PostForm("bot")
+	BotAdminModel.Api_select(bot, uid)
 }

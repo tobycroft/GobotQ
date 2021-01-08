43a04f9eee27b557b7a4bf04c41fdc79b834045d
Tuuz <miracle-plus@outlook.com>
2021-01-08 14:07:50 +0800

    TuuzMac

--- app/v1/bot/controller/operation.go
+++ app/v1/bot/controller/operation.go
@@ -1,7 +1,23 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/common/BaseController"
+)
 
 func OperationController(route *gin.RouterGroup) {
+
+	route.Use(BaseController.LoginedController(), gin.Recovery())
+
+	route.Any("online", operation_online)
+	route.Any("offline", operation_offline)
+
+}
+
+func operation_online(c *gin.Context) {
+
+}
+
+func operation_offline(c *gin.Context) {
 
 }

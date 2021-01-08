3a2fb689c6121240068dd98185fdcbdf2e6b9024
Tuuz <miracle-plus@outlook.com>
2021-01-08 17:24:57 +0800

    TuuzMac

--- app/v1/bot/controller/request.go
+++ app/v1/bot/controller/request.go
@@ -1,11 +1,16 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/common/BaseController"
+)
 
 func RequestController(route *gin.RouterGroup) {
 
+	route.Use(BaseController.LoginedController(), gin.Recovery())
 	route.Any("join", request_join)
 	route.Any("out", request_out)
+	route.Any("allow", request_out)
 }
 
 func request_join(c *gin.Context) {

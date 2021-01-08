2fae02ca6790aad7811f3391364d48040433b154
Tuuz <miracle-plus@outlook.com>
2021-01-08 14:59:14 +0800

    TuuzMac

--- app/v1/bot/controller/edit.go
+++ app/v1/bot/controller/edit.go
@@ -1,12 +1,20 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/tuuz/Input"
+)
 
 func EditController(route *gin.RouterGroup) {
 
+	route.Any("change_img", change_img)
 }
 
-func upload_img(c *gin.Context) {
+func change_img(c *gin.Context) {
 	uid := c.PostForm("uid")
+	img, ok := Input.Post("img", c, true)
+	if !ok {
+		return
+	}
 
 }

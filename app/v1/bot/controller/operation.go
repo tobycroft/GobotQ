0482e80c714000896c754d7f5596636d3575fd7e
Tuuz <miracle-plus@outlook.com>
2021-01-08 14:36:42 +0800

    TuuzMac

--- app/v1/bot/controller/operation.go
+++ app/v1/bot/controller/operation.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/app/bot/api"
 	"main.go/app/bot/model/BotModel"
 	"main.go/common/BaseController"
 	"main.go/tuuz/Input"
@@ -25,13 +26,38 @@ func operation_online(c *gin.Context) {
 	}
 	data := BotModel.Api_find_byOwnerandBot(uid, bot)
 	if len(data) > 0 {
+		ok, err := api.Logoutqq(bot)
+		if err != nil {
+			RET.Fail(c, 300, err.Error(), err.Error())
+		} else if ok {
+			RET.Success(c, 0, nil, nil)
+		} else {
+			RET.Fail(c, 300, nil, "下线失败，机器人可能本来就没有在线？")
+		}
 
 	} else {
 		RET.Fail(c, 403, nil, "你并不拥有这个机器人")
 	}
-
 }
 
 func operation_offline(c *gin.Context) {
+	uid := c.PostForm("uid")
+	bot, ok := Input.PostInt("bot", c)
+	if !ok {
+		return
+	}
+	data := BotModel.Api_find_byOwnerandBot(uid, bot)
+	if len(data) > 0 {
+		ok, err := api.Loginqq(bot)
+		if err != nil {
+			RET.Fail(c, 300, err.Error(), err.Error())
+		} else if ok {
+			RET.Success(c, 0, nil, nil)
+		} else {
+			RET.Fail(c, 300, nil, "上线失败，机器人的密码可能已经被修改了？")
+		}
 
+	} else {
+		RET.Fail(c, 403, nil, "你并不拥有这个机器人")
+	}
 }

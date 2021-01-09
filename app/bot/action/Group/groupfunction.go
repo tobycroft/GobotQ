a5f5115ea6710df8c928270086f1910c250b46b2
Tuuz <silverhawk@foxmail.com>
2021-01-09 19:45:19 +0800

    TuuzPC

--- app/bot/action/Group/groupfunction.go
+++ app/bot/action/Group/groupfunction.go
@@ -1,26 +1,56 @@
 package Group
 
 import (
-	"fmt"
+	"main.go/app/bot/api"
 	"main.go/app/bot/model/GroupFunctionDetailModel"
 	"main.go/app/bot/model/GroupFunctionModel"
+	"main.go/tuuz/Calc"
 )
 
 func App_group_function(bot *int, gid *int, uid *int) {
-	fmt.Println(group_function_attach(*gid))
+	settings := group_function_attach(*gid)
+	str := "您的群设定为：\r\n"
+	for _, v := range settings {
+		str += Calc.Any2String(v["name"]) + ":"
+		value := Calc.Any2String(v["value"])
+		switch v["type"] {
+		case "bool":
+			if value == "1" {
+				str += "是"
+			} else {
+				str += "否"
+			}
+			break
+
+		case "int":
+			str += value
+			break
+
+		case "string":
+			str += value
+			break
+
+		default:
+			str += value
+			break
+		}
+
+		str += "\r\n"
+	}
+	api.Sendgroupmsg(*bot, *gid, str)
 }
 
-func group_function_attach(gid interface{}) map[string]interface{} {
+func group_function_attach(gid interface{}) map[string]map[string]interface{} {
 	group_setting := GroupFunctionModel.Api_find(gid)
 	if len(group_setting) < 1 {
 		GroupFunctionModel.Api_insert(gid)
 		return group_function_attach(gid)
 	}
 	function := GroupFunctionDetailModel.Api_select_kv()
-	arr := make(map[string]interface{})
+	arr := make(map[string]map[string]interface{})
 	for k, v := range group_setting {
 		if function[k] != nil {
-			function[k].(map[string]interface{})["value"] = v
+			function[k]["value"] = v
 			arr[k] = function[k]
 		}
 	}

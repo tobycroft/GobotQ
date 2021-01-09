76cd22f646446abe17e5ebd74ef2bf4940b27b9e
Tuuz <silverhawk@foxmail.com>
2021-01-09 10:21:42 +0800

    TuuzPC

---
+++ app/bot/model/GroupFunctionModel/GroupFunctionModel.go
@@ -0,0 +1,39 @@
+package GroupFunctionModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "group_function"
+
+func Api_insert(gid interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"gid": gid,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_find(gid interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"gid": gid,
+	}
+	db.Where(where)
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}

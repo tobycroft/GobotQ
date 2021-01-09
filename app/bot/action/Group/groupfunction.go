2f2fce354aa00c7e7a4eded3eec8be065962f980
Tuuz <silverhawk@foxmail.com>
2021-01-09 19:33:44 +0800

    TuuzPC

--- app/bot/action/Group/groupfunction.go
+++ app/bot/action/Group/groupfunction.go
@@ -2,7 +2,6 @@ package Group
 
 import (
 	"fmt"
-	"github.com/gohouse/gorose/v2"
 	"main.go/app/bot/model/GroupFunctionDetailModel"
 	"main.go/app/bot/model/GroupFunctionModel"
 )
@@ -11,18 +10,19 @@ func App_group_function(bot *int, gid *int, uid *int) {
 	fmt.Println(group_function_attach(*gid))
 }
 
-func group_function_attach(gid interface{}) gorose.Data {
+func group_function_attach(gid interface{}) map[string]interface{} {
 	group_setting := GroupFunctionModel.Api_find(gid)
 	if len(group_setting) < 1 {
 		GroupFunctionModel.Api_insert(gid)
 		return group_function_attach(gid)
 	}
 	function := GroupFunctionDetailModel.Api_select_kv()
+	arr := make(map[string]interface{})
 	for k, v := range group_setting {
 		if function[k] != nil {
-			function[k].(gorose.Data)["value"] = v
-			group_setting[k] = function[k]
+			function[k].(map[string]interface{})["value"] = v
+			arr[k] = function[k]
 		}
 	}
-	return group_setting
+	return arr
 }

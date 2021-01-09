eebd4a240f8f2f04285f8a88991a08da2b4ef27d
Tuuz <silverhawk@foxmail.com>
2021-01-09 12:18:45 +0800

    TuuzPC

--- app/bot/model/GroupFunctionDetailModel/GroupFunctionDetailModel.go
+++ app/bot/model/GroupFunctionDetailModel/GroupFunctionDetailModel.go
@@ -1,23 +1,53 @@
 package GroupFunctionDetailModel
 
 import (
+	"github.com/gohouse/gorose/v2"
 	"main.go/tuuz"
 	"main.go/tuuz/Log"
 )
 
 const table = "group_function_detail"
 
-func Api_find_byK(k interface{}) interface{} {
+func Api_find_byK(key interface{}) interface{} {
 	db := tuuz.Db().Table(table)
 	where := map[string]interface{}{
-		"k": k,
+		"key": key,
 	}
 	db.Where(where)
-	ret, err := db.Value("v")
+	ret, err := db.Value("name")
 	if err != nil {
 		Log.Dbrr(err, tuuz.FUNCTION_ALL())
 		return nil
 	} else {
 		return ret
+	}
+}
+
+func Api_select() []gorose.Data {
+	db := tuuz.Db().Table(table)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_select_kv() map[string]interface{} {
+	db := tuuz.Db().Table(table)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		datas := map[string]interface{}{}
+		for _, data := range ret {
+			datas[data["key"].(string)] = map[string]interface{}{
+				"value": data["value"],
+				"type":  data["type"],
+			}
+		}
+		return datas
 	}
 }

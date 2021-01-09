1e6667362556ba75c3b6ccac7fb75ae6def13e19
Tuuz <silverhawk@foxmail.com>
2021-01-09 19:49:52 +0800

    TuuzPC

--- app/bot/service/text_handle.go
+++ app/bot/service/text_handle.go
@@ -16,6 +16,18 @@ func Serv_text_match(text string, Case []string) (string, bool) {
 	return "", false
 }
 
+func Serv_text_match_all(text string, Case []string) (string, bool) {
+	for _, str := range Case {
+		reg := regexp.MustCompile("(?i)^" + str + "$")
+		active := reg.MatchString(text)
+		if active {
+			new_text := reg.ReplaceAllString(text, "")
+			return new_text, true
+		}
+	}
+	return "", false
+}
+
 func Serv_text_match_define(text *string, Case []string) bool {
 	for _, str := range Case {
 		reg := regexp.MustCompile("(?i)^" + str)

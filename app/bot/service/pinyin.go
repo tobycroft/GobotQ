559fc32b80f40799189119200f2d97249330bcc5
Tuuz <silverhawk@foxmail.com>
2021-01-10 16:41:51 +0800

    TuuzPC

--- app/bot/service/pinyin.go
+++ app/bot/service/pinyin.go
@@ -1,7 +1,27 @@
 package service
 
-import "main.go/tuuz/Net"
+import (
+	jsoniter "github.com/json-iterator/go"
+	"main.go/tuuz/Calc"
+	"main.go/tuuz/Net"
+)
 
-func Serv_pinyin() {
-	pinyin, err := Net.Rpc()
+type py struct {
+	Errno  int    `json:"errno"`
+	Errmsg string `json:"errmsg"`
+	Data   string `json:"data"`
+}
+
+func Serv_pinyin(chinese interface{}) (string, error) {
+	data, err := Net.PostRaw("http://www.box3.cn/developtoolbox/pinyin.php", Calc.Any2String(chinese))
+	if err != nil {
+		return "", err
+	}
+	var ret py
+	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	err = jsr.UnmarshalFromString(data, &ret)
+	if err != nil {
+		return "", err
+	}
+	return ret.Data, nil
 }

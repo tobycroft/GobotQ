e75ad538d713aa6ee61a9e0e429d537e7d779c79
Tuuz <silverhawk@foxmail.com>
2021-01-10 17:31:50 +0800

    TuuzPC

--- app/bot/api/deletegroupmsg.go
+++ app/bot/api/deletegroupmsg.go
@@ -10,6 +10,16 @@ type DeleteGroupMsgRet struct {
 	Ret string `json:"ret"`
 }
 
+type Retract_group struct {
+	Fromqq interface{}
+	Group  interface{}
+	Random interface{}
+	Req    interface{}
+}
+
+var Retract_chan_group = make(chan Retract_group, 20)
+var Retract_chan_group_instant = make(chan Retract_group, 20)
+
 func Deletegroupmsg(fromqq, group, random, req interface{}) (bool, error) {
 	post := map[string]interface{}{
 		"fromqq": fromqq,

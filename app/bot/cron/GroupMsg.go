4f150b6017a62d494cba9da729c2ca43bcf74d29
Tuuz <silverhawk@foxmail.com>
2021-01-10 17:05:43 +0800

    TuuzPC

--- app/bot/cron/GroupMsg.go
+++ app/bot/cron/GroupMsg.go
@@ -8,6 +8,6 @@ import (
 func GroupMsgRecv() {
 	for gm := range event.GroupMsgChan {
 		GroupMsgModel.Api_insert(gm.LogonQQ, gm.FromQQ.UIN, gm.FromGroup.GIN, gm.Msg.Text, gm.Msg.Req, gm.Msg.Random, gm.File.ID, gm.File.MD5,
-			gm.File.Name, gm.File.Size)
+			gm.File.Name, gm.File.Size, gm.TimeStamp.Send, gm.TimeStamp.Recv)
 	}
 }

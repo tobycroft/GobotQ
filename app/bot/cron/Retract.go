62f873f37a4ca9b5b25266db86da41a938905e1d
Tuuz <silverhawk@foxmail.com>
2021-01-10 15:55:55 +0800

    TuuzPC

--- app/bot/cron/Retract.go
+++ app/bot/cron/Retract.go
@@ -2,30 +2,10 @@ package cron
 
 import (
 	"main.go/app/bot/api"
+	"main.go/app/bot/event"
 	"main.go/config/app_conf"
 	"time"
 )
-
-type Retract_private struct {
-	Fromqq interface{}
-	Toqq   interface{}
-	Random interface{}
-	Req    interface{}
-	Time   interface{}
-}
-
-type Retract_group struct {
-	Fromqq interface{}
-	Group  interface{}
-	Random interface{}
-	Req    interface{}
-}
-
-var Retract_chan_private = make(chan Retract_private, 20)
-var Retract_chan_group = make(chan Retract_group, 20)
-
-var Retract_chan_private_instant = make(chan Retract_private, 20)
-var Retract_chan_group_instant = make(chan Retract_group, 20)
 
 func Retract() {
 	go retract_private()
@@ -35,11 +15,11 @@ func Retract() {
 }
 
 func retract_private() {
-	for r := range Retract_chan_private {
+	for r := range event.Retract_chan_private {
 		go func() {
 			time.Sleep(app_conf.Retract_time_second * time.Second)
 			select {
-			case Retract_chan_private_instant <- r:
+			case event.Retract_chan_private_instant <- r:
 
 			case <-time.After(5 * time.Second):
 				return
@@ -49,11 +29,11 @@ func retract_private() {
 }
 
 func retract_group() {
-	for r := range Retract_chan_group {
+	for r := range event.Retract_chan_group {
 		go func() {
 			time.Sleep(app_conf.Retract_time_second * time.Second)
 			select {
-			case Retract_chan_group_instant <- r:
+			case event.Retract_chan_group_instant <- r:
 
 			case <-time.After(5 * time.Second):
 				return
@@ -63,13 +43,13 @@ func retract_group() {
 }
 
 func retract_private_instant() {
-	for r := range Retract_chan_private_instant {
+	for r := range event.Retract_chan_private_instant {
 		api.Deleteprivatemsg(r.Fromqq, r.Toqq, r.Random, r.Req, r.Time)
 	}
 }
 
 func retract_group_instant() {
-	for r := range Retract_chan_group_instant {
+	for r := range event.Retract_chan_group_instant {
 		api.Deletegroupmsg(r.Fromqq, r.Group, r.Random, r.Req)
 	}
 }

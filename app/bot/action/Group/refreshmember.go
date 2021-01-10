4315c6fa0c89febae098e5d242febf0f61168c8e
Tuuz <silverhawk@foxmail.com>
2021-01-10 12:50:23 +0800

    TuuzPC

--- app/bot/action/Group/refreshmember.go
+++ app/bot/action/Group/refreshmember.go
@@ -4,8 +4,8 @@ import "main.go/app/bot/api"
 
 func App_refreshmember(bot *int, gid *int, uid *int) {
 	var apm App_group_member
-	apm.Bot = bot
-	apm.Gid = gid
+	apm.Bot = *bot
+	apm.Gid = *gid
 	Chan_refresh_group_member <- apm
 	api.Sendgroupmsg(*bot, *gid, "群用户已经刷新")
 }

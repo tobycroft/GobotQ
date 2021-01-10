61c8b674f932f7f6cb2a6c89037f11a89d5227af
Tuuz <silverhawk@foxmail.com>
2021-01-10 18:51:05 +0800

    TuuzPC

---
+++ app/bot/cron/Time.go
@@ -0,0 +1,44 @@
+package cron
+
+import (
+	"main.go/app/bot/action/Group"
+	"main.go/app/bot/api"
+	"main.go/app/bot/model/BotModel"
+	"main.go/app/bot/model/GroupFunctionModel"
+	"main.go/app/bot/model/GroupListModel"
+	"time"
+)
+
+func BaseCron() {
+	for {
+		time.Sleep(3600 * time.Second)
+		bots := BotModel.Api_select()
+		for _, bot := range bots {
+			gl, err := api.Getgrouplist(bot["bot"])
+			if err != nil {
+
+			} else {
+				GroupListModel.Api_delete(bot["bot"])
+				var gss []GroupListModel.GroupList
+				for _, gll := range gl {
+					var gs GroupListModel.GroupList
+					gs.Bot = bot["bot"]
+					gs.Gid = gll.GIN
+					gs.Group_name = gll.StrGroupName
+					gs.Group_memo = gll.StrGroupMemo
+					gs.Owner = gll.DwGroupOwnerUin
+					gs.Number = gll.DwMemberNum
+					gss = append(gss, gs)
+					var gm Group.App_group_member
+					gm.Gid = gll.GIN
+					gm.Bot = bot["bot"]
+					Group.Chan_refresh_group_member <- gm
+					if len(GroupFunctionModel.Api_find(gll.GIN)) < 1 {
+						GroupFunctionModel.Api_insert(gll.GIN)
+					}
+				}
+				GroupListModel.Api_insert_more(gss)
+			}
+		}
+	}
+}

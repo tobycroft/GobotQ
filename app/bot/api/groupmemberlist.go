a1e807c54abd11a24277b8dffc0962593bef41d7
Tuuz <silverhawk@foxmail.com>
2021-01-10 12:21:19 +0800

    TuuzPC

--- app/bot/api/groupmemberlist.go
+++ app/bot/api/groupmemberlist.go
@@ -4,22 +4,23 @@ import (
 	"fmt"
 	jsoniter "github.com/json-iterator/go"
 	"main.go/config/app_conf"
+	"main.go/tuuz/Jsong"
 	"main.go/tuuz/Net"
 )
 
 type Gms struct {
 	Ret  string            `json:"ret"`
-	List []GroupMemberList `json:"List"`
+	List []GroupMemberList `json:"List" bson:"List"`
 }
 type GroupMemberList struct {
 	UIN              int    `json:"UIN"`
 	Age              int    `json:"Age"`
 	Sex              int    `json:"Sex"`
-	NickName         string `json:"NickName"`
-	Email            string `json:"Email"`
-	Card             string `json:"Card"`
-	Remark           string `json:"Remark"`
-	SpecTitle        string `json:"SpecTitle"`
+	NickName         string `json:"NickName" bson:"NickName"`
+	Email            string `json:"Email" bson:"Email"`
+	Card             string `json:"Card" bson:"Card"`
+	Remark           string `json:"Remark" bson:"Remark"`
+	SpecTitle        string `json:"SpecTitle" bson:"SpecTitle"`
 	Phone            string `json:"Phone"`
 	SpecTitleExpired int    `json:"SpecTitleExpired"`
 	MuteTime         int    `json:"MuteTime"`
@@ -28,7 +29,7 @@ type GroupMemberList struct {
 	GroupLevel       int    `json:"GroupLevel"`
 }
 
-func Getgroupmemberlist(bot, group interface{}) ([]GroupMemberList, error) {
+func Getgroupmemberlist(bot, group interface{}) (interface{}, error) {
 	post := map[string]interface{}{
 		"logonqq": bot,
 		"group":   group,
@@ -40,9 +41,10 @@ func Getgroupmemberlist(bot, group interface{}) ([]GroupMemberList, error) {
 	fmt.Println(data)
 	var gms Gms
 	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	Jsong.JArray()
 	err = jsr.UnmarshalFromString(data, &gms)
 	if err != nil {
 		return nil, err
 	}
-	return gms.List, nil
+	return nil, nil
 }

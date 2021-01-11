967bed342b0c4e05ab84ca5c5cf5a9c1a98575a0
Tuuz <silverhawk@foxmail.com>
2021-01-12 01:31:07 +0800

    TuuzPC

--- app/bot/service/str.go
+++ app/bot/service/str.go
@@ -2,6 +2,6 @@ package service
 
 import "main.go/app/bot/api"
 
-func Not_admin(bot *int, gid *int, uid *int) {
-	api.Sendgroupmsg(*bot, *gid, "你不是本群的管理员，无法使用本功能"+Serv_at(*uid), true)
+func Not_admin(bot int, gid int, uid int) {
+	api.Sendgroupmsg(bot, gid, "你不是本群的管理员，无法使用本功能"+Serv_at(uid), true)
 }

5afa0e69bbee8327df03226b0db4ab299c1789b2
Tuuz <miracle-plus@outlook.com>
2021-01-11 12:22:17 +0800

    TuuzMac

---
+++ app/bot/api/getgroupmgrlist.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	jsoniter "github.com/json-iterator/go"
+	"main.go/config/app_conf"
+	"main.go/tuuz/Input"
+	"main.go/tuuz/Net"
+	"strings"
+)
+
+type GroupAdminList struct {
+	Ret string `json:"ret"`
+}
+
+func Getgroupmgrlist(bot interface{}) ([]string, error) {
+	post := map[string]interface{}{
+		"logonqq": bot,
+	}
+	data, err := Net.Post(app_conf.Http_Api+"/getgrouplist", nil, post, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	data = Input.Fliter_Ascii(data)
+	data = Input.Fliter_error_encode(data)
+	var ret GroupAdminList
+	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	err = jsr.UnmarshalFromString(data, &ret)
+	if err != nil {
+		return nil, err
+	}
+	strs := strings.Split(ret.Ret, "\r\n")
+	return strs, nil
+}

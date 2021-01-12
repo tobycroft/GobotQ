e2b8be6d7d683fdc1465f38b75227591e0d78ff8
Tuuz <silverhawk@foxmail.com>
2021-01-12 23:14:11 +0800

    TuuzPC

---
+++ app/bot/action/Group/user_operation.go
@@ -0,0 +1,13 @@
+package Group
+
+func App_ban_user(bot, gid, uid int, req int, random int, groupmember map[string]interface{}, groupfunction map[string]interface{}) {
+
+}
+
+func Api_kick_user(bot, gid, uid int, req int, random int, groupmember map[string]interface{}, groupfunction map[string]interface{}) {
+
+}
+
+func Api_retract_send(bot, gid, uid int, req int, random int, groupmember map[string]interface{}, groupfunction map[string]interface{}) {
+
+}

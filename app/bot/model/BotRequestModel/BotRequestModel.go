1557e598591c7fa6faa997d053737164457a5798
Tuuz <silverhawk@foxmail.com>
2021-01-12 20:30:27 +0800

    TuuzPC

---
+++ app/bot/model/BotRequestModel/BotRequestModel.go
@@ -0,0 +1,70 @@
+package BotRequestModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "bot_request"
+
+func Api_insert(uid, bot, password, owner, secret interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"uid":      uid,
+		"bot":      bot,
+		"password": password,
+		"owner":    owner,
+		"secret":   secret,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_find(bot, password interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"bot":      bot,
+		"password": password,
+	}
+	db.Where(where)
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_select() []gorose.Data {
+	db := tuuz.Db().Table(table)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_delete(bot interface{}) bool {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"bot": bot,
+	}
+	db.Where(where)
+	_, err := db.Delete()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}

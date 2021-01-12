8a38ca52266b15b7dfa6a3c8cc5d58252c9e57d9
Tuuz <silverhawk@foxmail.com>
2021-01-12 23:34:52 +0800

    TuuzPC

--- app/bot/service/url.go
+++ app/bot/service/url.go
@@ -2,12 +2,7 @@ package service
 
 import "regexp"
 
-func Serv_url_detect(text string) (string, bool) {
-	str := "[http].*.[com|cn|ltd|xyz|net|xyz|top|tech|org|gov|edu|ink|int|pub|cc|info|io]"
-	reg := regexp.MustCompile(str)
-	active := reg.MatchString(text)
-	if active {
-		new_text := reg.ReplaceAllString(text, "")
-		return new_text, true
-	}
+func Serv_url_detect(text string) bool {
+	c1, _ := regexp.MatchString("[http].*.[com|cn|ltd|xyz|net|xyz|top|tech|org|gov|edu|ink|int|pub|cc|info|io]", text)
+	return c1
 }

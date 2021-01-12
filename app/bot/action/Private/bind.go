4553a47e37d1e1b7d80525506df9aa58317019e2
Tuuz <silverhawk@foxmail.com>
2021-01-12 21:12:44 +0800

    TuuzPC

--- app/bot/action/Private/bind.go
+++ app/bot/action/Private/bind.go
@@ -1,5 +1,35 @@
 package Private
 
-func App_bind_robot(bot *int, uid *int, text string) {
+import (
+	"main.go/app/bot/api"
+	"main.go/app/bot/model/BotModel"
+	"main.go/app/bot/model/BotRequestModel"
+	"main.go/tuuz"
+	"time"
+)
 
+func App_bind_robot(bot int, uid int, text string) {
+	data := BotRequestModel.Api_find(bot, text)
+	if len(data) > 0 {
+		db := tuuz.Db()
+		db.Begin()
+		var botreq BotRequestModel.Interface
+		botreq.Db = db
+		if !botreq.Api_delete(bot) {
+			db.Rollback()
+			return
+		}
+		if data {
+
+		}
+		if BotModel.Api_insert(bot, bot, "private", uid, data["secret"], data["password"], time.Now().Unix()+data["time"].(int64)) {
+			db.Commit()
+			api.Sendprivatemsg(bot, uid, "你已经成功绑定这个机器人咯！", false)
+		} else {
+			db.Rollback()
+			api.Sendprivatemsg(bot, uid)
+		}
+	} else {
+		api.Sendprivatemsg(bot, uid, "未找到这个机器人", true)
+	}
 }

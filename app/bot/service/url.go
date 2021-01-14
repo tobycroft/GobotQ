1dbade65e19a3a2d6b4422f8238d22709c49929a
Tuuz <miracle-plus@outlook.com>
2021-01-14 17:28:40 +0800

    TuuzMac

--- app/bot/service/url.go
+++ app/bot/service/url.go
@@ -3,6 +3,10 @@ package service
 import "regexp"
 
 func Serv_url_detect(text string) bool {
-	c1, _ := regexp.MatchString("[http].*.[com|cn|ltd|xyz|net|xyz|top|tech|org|gov|edu|ink|int|pub|cc|info|io]", text)
+	c1, _ := regexp.MatchString("[http]+", text)
+	if c1 {
+		c2, _ := regexp.MatchString("[com|cn|ltd|xyz|net|xyz|top|tech|org|gov|edu|ink|int|pub|cc|info|io]", text)
+		return c2
+	}
 	return c1
 }

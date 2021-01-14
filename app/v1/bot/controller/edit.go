5e0ab99d402aa6c193c053f5d6effedff2c88419
Tuuz <miracle-plus@outlook.com>
2021-01-14 15:42:22 +0800

    TuuzMac

--- app/v1/bot/controller/edit.go
+++ app/v1/bot/controller/edit.go
@@ -3,12 +3,29 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"main.go/app/bot/model/BotModel"
+	"main.go/common/BaseController"
 	"main.go/tuuz/Input"
 	"main.go/tuuz/RET"
 )
 
 func EditController(route *gin.RouterGroup) {
-
+	route.Use(BaseController.LoginedController(), gin.Recovery())
+	route.Use(func(c *gin.Context) {
+		uid := c.PostForm("uid")
+		bot, ok := Input.PostInt("bot", c)
+		if !ok {
+			return
+		}
+		data := BotModel.Api_find_byOwnerandBot(uid, bot)
+		if len(data) > 0 {
+			c.Next()
+			return
+		} else {
+			RET.Fail(c, 403, nil, "你并不拥有这个机器人")
+			c.Abort()
+			return
+		}
+	})
 	route.Any("change_img", change_img)
 }
 
@@ -24,4 +41,8 @@ func change_img(c *gin.Context) {
 	} else {
 		RET.Fail(c, 0, nil, nil)
 	}
+}
+
+func change_name(c *gin.Context) {
+	uid := c.PostForm("uid")
 }

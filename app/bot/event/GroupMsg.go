ad9f0e847d7937456c9d955466ae40ef8480791a
Tuuz <miracle-plus@outlook.com>
2021-01-15 11:51:14 +0800

    TuuzMac

--- app/bot/event/GroupMsg.go
+++ app/bot/event/GroupMsg.go
@@ -3,7 +3,6 @@ package event
 import (
 	"main.go/app/bot/action/Group"
 	"main.go/app/bot/api"
-	"main.go/app/bot/model/GroupBalanceModel"
 	"main.go/app/bot/model/GroupBanModel"
 	"main.go/app/bot/model/GroupFunctionModel"
 	"main.go/app/bot/model/GroupMemberModel"
@@ -344,25 +343,11 @@ func groupHandle_acfur_other(Type string, bot *int, gid *int, uid *int, text str
 		break
 
 	case "积分查询":
-		gbl := GroupBalanceModel.Api_find(*gid, *uid)
-		str := "您当前拥有" + Calc.Any2String(gbl["balance"]) + "分"
-		api.Sendgroupmsg(*bot, *gid, str, true)
+		Group.App_check_balance(*bot, *gid, *uid, *req, *random, groupmember, groupfunction)
 		break
 
 	case "积分排行":
-		gbl := GroupBalanceModel.Api_select(*gid, 10)
-		str := ""
-		for i1, i2 := range gbl {
-			user := GroupMemberModel.Api_find(*gid, i2["uid"].(int64))
-			if len(user) > 0 {
-				if len(Calc.Any2String(user["card"])) > 2 && Calc.Any2String(user["card"]) != "null" {
-					str += "第" + Calc.Int2String(i1+1) + "名：" + user["card"].(string) + "，" + Calc.Any2String(i2["balance"]) + "分" + "\r\n"
-				} else {
-					str += "第" + Calc.Int2String(i1+1) + "名：" + user["nickname"].(string) + "，" + Calc.Any2String(i2["balance"]) + "分" + "\r\n"
-				}
-			}
-		}
-		api.Sendgroupmsg(*bot, *gid, str, true)
+		Group.App_check_rank(*bot, *gid, *uid, *req, *random, groupmember, groupfunction)
 		break
 
 	default:

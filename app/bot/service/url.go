9739be1227f5c051cf5b3938d37d53cda59893a9
Tuuz <silverhawk@foxmail.com>
2021-01-17 20:38:52 +0800

    TuuzPC

--- app/bot/service/url.go
+++ app/bot/service/url.go
@@ -1,12 +1,14 @@
 package service
 
-import "regexp"
+import (
+	"regexp"
+	"strings"
+)
 
 func Serv_url_detect(text string) bool {
-	c1, _ := regexp.MatchString("[http]+", text)
-	if c1 {
-		c2, _ := regexp.MatchString("[com|cn|ltd|xyz|net|xyz|top|tech|org|gov|edu|ink|int|pub|cc|info|io]", text)
+	if strings.Contains(text, "http") {
+		c2, _ := regexp.MatchString("\\.com+|\\.cn+|\\.ltd+|\\.xyz+|\\.net+|\\.xyz+|\\.top+|\\.tech+|\\.org+|\\.gov+|\\.edu+|\\.ink+|\\.int+|\\.pub+|\\.cc+|\\.info+|\\.io+", text)
 		return c2
 	}
-	return c1
+	return false
 }

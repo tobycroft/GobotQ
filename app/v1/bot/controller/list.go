d209ffb1457caeb1006c0ef22e2dd0f10db4e826
Tuuz <silverhawk@foxmail.com>
2021-01-21 20:44:42 +0800

    TuuzPC

--- app/v1/bot/controller/list.go
+++ app/v1/bot/controller/list.go
@@ -5,28 +5,11 @@ import (
 	"main.go/app/bot/model/BotModel"
 	"main.go/app/v1/bot/model/BotAdminModel"
 	"main.go/common/BaseController"
-	"main.go/tuuz/Input"
 	"main.go/tuuz/RET"
 )
 
 func ListController(route *gin.RouterGroup) {
 	route.Use(BaseController.LoginedController(), gin.Recovery())
-	route.Use(func(c *gin.Context) {
-		uid := c.PostForm("uid")
-		bot, ok := Input.PostInt("bot", c)
-		if !ok {
-			return
-		}
-		data := BotModel.Api_find_byOwnerandBot(uid, bot)
-		if len(data) > 0 {
-			c.Next()
-			return
-		} else {
-			RET.Fail(c, 403, nil, "你并不拥有这个机器人")
-			c.Abort()
-			return
-		}
-	})
 	route.Any("unbind", list_unbind)
 	route.Any("owned", list_yours_own)
 	route.Any("admin", list_your_admin_group)

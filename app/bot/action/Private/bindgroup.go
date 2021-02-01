f77e63a326c9ce3b22451eecb7264caeb2df4cc0
Tuuz <silverhawk@foxmail.com>
2021-02-01 23:08:55 +0800

    TuuzPC

--- app/bot/action/Private/bindgroup.go
+++ app/bot/action/Private/bindgroup.go
@@ -10,6 +10,10 @@ func App_bind_group(bot int, uid int, text string) {
 		api.Sendprivatemsg(bot, uid, "请使用\"acfur绑定群群号\"，来绑定新群", false)
 		return
 	}
+	if len(BotGroupAllowModel.Api_find(bot, text)) > 0 {
+		api.Sendprivatemsg(bot, uid, "群号已经被绑定："+text, false)
+		return
+	}
 	if BotGroupAllowModel.Api_insert(bot, text) {
 		api.Sendprivatemsg(bot, uid, "绑定群已经增加："+text, false)
 	} else {

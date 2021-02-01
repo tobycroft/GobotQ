1000dcb6f956ba7aecc145010208bab6d1c5f902
Tuuz <silverhawk@foxmail.com>
2021-02-01 22:59:22 +0800

    TuuzPC

---
+++ app/bot/action/Private/bindgroup.go
@@ -0,0 +1,30 @@
+package Private
+
+import (
+	"main.go/app/bot/api"
+	"main.go/app/bot/model/BotGroupAllowModel"
+)
+
+func App_bind_group(bot int, uid int, text string) {
+	if len(text) < 2 {
+		api.Sendprivatemsg(bot, uid, "请使用\"acfur绑定群:群号\"，来绑定新群", false)
+		return
+	}
+	if BotGroupAllowModel.Api_insert(bot, text) {
+		api.Sendprivatemsg(bot, uid, "绑定群已经增加："+text, false)
+	} else {
+		api.Sendprivatemsg(bot, uid, "绑定群增加失败："+text, false)
+	}
+}
+
+func App_unbind_group(bot int, uid int, text string) {
+	if len(text) < 2 {
+		api.Sendprivatemsg(bot, uid, "请使用\"acfur绑定群:群号\"，来绑定新群", false)
+		return
+	}
+	if BotGroupAllowModel.Api_insert(bot, text) {
+		api.Sendprivatemsg(bot, uid, "绑定群已经增加："+text, false)
+	} else {
+		api.Sendprivatemsg(bot, uid, "绑定群增加失败："+text, false)
+	}
+}

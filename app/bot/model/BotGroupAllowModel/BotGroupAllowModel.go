0d531dcc154d8cf07cec99cd74fca9fb5377ba97
Tuuz <silverhawk@foxmail.com>
2021-02-01 19:33:12 +0800

    TuuzPC

---
+++ app/bot/model/BotGroupAllowModel/BotGroupAllowModel.go
@@ -0,0 +1,56 @@
+package BotGroupAllowModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "bot_group_allow"
+
+func Api_insert(bot, gid interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"bot": bot,
+		"gid": gid,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_find(bot, gid interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"bot": bot,
+		"gid": gid,
+	}
+	db.Where(where)
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_select(bot interface{}) []gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"bot": bot,
+	}
+	db.Where(where)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}

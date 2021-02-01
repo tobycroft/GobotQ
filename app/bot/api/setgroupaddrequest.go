b29fb19e3e56eca27877cf137dca414b0a850b93
Tuuz <silverhawk@foxmail.com>
2021-02-01 21:58:27 +0800

    TuuzPC

--- app/bot/api/setgroupaddrequest.go
+++ app/bot/api/setgroupaddrequest.go
@@ -13,14 +13,17 @@ type SetGroupAddRequestRet struct {
 const Request_group_denide = 12
 const Request_group_approve = 11
 
-func Setgroupaddrequest(fromqq, group, qq, seq, Request_group_, Type, reason interface{}) (bool, error) {
+const Request_group_type_invite = 1
+const Request_group_type_join = 3
+
+func Setgroupaddrequest(fromqq, group, qq, seq, Request_group_, Request_group_type_, reason interface{}) (bool, error) {
 	post := map[string]interface{}{
 		"fromqq": fromqq,
 		"group":  group,
 		"qq":     qq,
 		"seq":    seq,
 		"op":     Request_group_,
-		"type":   Type,
+		"type":   Request_group_type_,
 		"reason": reason,
 	}
 	data, err := Net.Post(app_conf.Http_Api+"/setgroupaddrequest", nil, post, nil, nil)

b14af23737560344396e39fcc823afecb8f0b51a
Tuuz <silverhawk@foxmail.com>
2021-02-01 23:11:59 +0800

    TuuzPC

--- app/bot/event/PrivateMsg.go
+++ app/bot/event/PrivateMsg.go
@@ -226,6 +226,7 @@ func privateHandle_acfur_middle(bot *int, uid *int, text, origin_text string) {
 }
 
 func privateHandle_acfur_other(Type string, bot *int, uid *int, text string) {
+	botinfo := BotModel.Api_find(*bot)
 	switch Type {
 	case "密码":
 		Private.App_userChangePassword(*bot, *uid, text)
@@ -240,11 +241,19 @@ func privateHandle_acfur_other(Type string, bot *int, uid *int, text string) {
 		break
 
 	case "绑定群":
-		Private.App_bind_group(*bot, *uid, text)
+		if *uid == botinfo["owner"] {
+			Private.App_bind_group(*bot, *uid, text)
+		} else {
+			api.Sendprivatemsg(*bot, *uid, "您为拥有这个机器人的权限，请先绑定机器人", true)
+		}
 		break
 
 	case "解绑群":
-		Private.App_unbind_group(*bot, *uid, text)
+		if *uid == botinfo["owner"] {
+			Private.App_unbind_group(*bot, *uid, text)
+		} else {
+			api.Sendprivatemsg(*bot, *uid, "您为拥有这个机器人的权限，请先绑定机器人", true)
+		}
 		break
 
 	default:

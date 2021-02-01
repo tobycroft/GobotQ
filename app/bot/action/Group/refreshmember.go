442ee77224bbc63d287de118846f91d37edd6b5e
Tuuz <silverhawk@foxmail.com>
2021-02-01 19:55:36 +0800

    TuuzPC

--- app/bot/action/Group/refreshmember.go
+++ app/bot/action/Group/refreshmember.go
@@ -7,5 +7,5 @@ func App_refreshmember(bot *int, gid *int) {
 	apm.Bot = *bot
 	apm.Gid = *gid
 	Chan_refresh_group_member <- apm
-	api.Sendgroupmsg(*bot, *gid, "群用户已经刷新", false)
+	api.Sendgroupmsg(*bot, *gid, "群用户已经刷新", true)
 }

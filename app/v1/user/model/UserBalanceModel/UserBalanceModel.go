a4c747d3dcedb6d0008f07e240ffb5f2e0b58c10
Tuuz <silverhawk@foxmail.com>
2021-02-03 21:37:59 +0800

    TuuzPC

---
+++ app/v1/user/model/UserBalanceModel/UserBalanceModel.go
@@ -0,0 +1,65 @@
+package UserBalanceModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "user_balance"
+
+func Api_insert(qq, balance interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"qq":      qq,
+		"balance": balance,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_find(qq interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"qq": qq,
+	}
+	db.Where(where)
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+type Interface struct {
+	Db gorose.IOrm
+}
+
+func Api_dec_balance(qq interface{}, balance_dec float64) bool {
+	var self Interface
+	self.Db = tuuz.Db()
+	return self.Api_dec_balance(qq, balance_dec)
+}
+
+func (self *Interface) Api_dec_balance(qq interface{}, balance_dec float64) bool {
+	db := self.Db.Table(table)
+	where := map[string]interface{}{
+		"qq": qq,
+	}
+	db.Where(where)
+	_, err := db.Decrement("balance", balance_dec)
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}

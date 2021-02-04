a638b63eec404fddda9d70e717a9650ed34df3a6
Tuuz <silverhawk@foxmail.com>
2021-02-04 20:26:10 +0800

    TuuzPC

--- app/v1/group/controller/edit.go
+++ app/v1/group/controller/edit.go
@@ -1,7 +1,61 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/app/bot/model/GroupFunctionModel"
+	"main.go/app/bot/model/GroupMemberModel"
+	"main.go/common/BaseController"
+	"main.go/tuuz/Input"
+	"main.go/tuuz/RET"
+)
 
 func EditController(route *gin.RouterGroup) {
+	route.Use(BaseController.LoginedController(), gin.Recovery())
+	route.Use(func(c *gin.Context) {
+		uid := c.PostForm("uid")
+		gid, ok := Input.PostInt64("gid", c)
+		if !ok {
+			return
+		}
+		data := GroupMemberModel.Api_find(gid, uid)
+		if len(data) > 0 {
+			if data["type"].(string) == "admin" || data["type"].(string) == "owner" {
+				RET.Success(c, 0, data, nil)
+				c.Next()
+				return
+			} else {
+				RET.Fail(c, 403, nil, "你不是本群的管理员")
+				c.Abort()
+				return
+			}
+		} else {
+			RET.Fail(c, 403, nil, "未找到该群，请检查机器人是否有加入本群")
+			c.Abort()
+			return
+		}
+	})
+
+	route.Any("setting", group_setting_set)
+	route.Any("setting_get", group_setting_get)
+}
+
+func group_setting_set(c *gin.Context) {
+	gid, ok := Input.PostInt64("gid", c)
+	if !ok {
+		return
+	}
+	key, ok := Input.Post("key", c, false)
+	if !ok {
+		return
+	}
+	value, ok := Input.Post("value", c, false)
+	if !ok {
+		return
+	}
+	if GroupFunctionModel.Api_update(gid, key, value) {
+		RET.Success(c, 0, nil, "修改成功")
+	} else {
+		RET.Fail(c, 500, nil, "数据库修改失败")
+	}
 
 }
--- app/v1/group/controller/list.go
+++ app/v1/group/controller/list.go
@@ -2,9 +2,12 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"main.go/app/bot/model/GroupFunctionDetailModel"
+	"main.go/app/bot/model/GroupFunctionModel"
 	"main.go/app/bot/model/GroupListModel"
 	"main.go/app/bot/model/GroupMemberModel"
 	"main.go/common/BaseController"
+	"main.go/tuuz/Input"
 	"main.go/tuuz/RET"
 )
 
@@ -13,8 +16,9 @@ func ListController(route *gin.RouterGroup) {
 	route.Use(BaseController.LoginedController())
 
 	route.Any("control", group_control)
-	route.Any("joined", group_control)
+	route.Any("joined", group_joined)
 	route.Any("member", group_member)
+	route.Any("setting", group_setting_get)
 }
 
 func group_control(c *gin.Context) {
@@ -28,12 +32,46 @@ func group_control(c *gin.Context) {
 	RET.Success(c, 0, gls, nil)
 }
 
-func group_joined() {
-
+func group_joined(c *gin.Context) {
+	uid := c.PostForm("uid")
+	con_group := GroupMemberModel.Api_select_byUid(uid, []interface{}{"member"})
+	gids := []interface{}{}
+	for _, data := range con_group {
+		gids = append(gids, data["gid"])
+	}
+	gls := GroupListModel.Api_select_InGid(gids)
+	RET.Success(c, 0, gls, nil)
 }
 
 func group_member(c *gin.Context) {
 	uid := c.PostForm("uid")
 	usergroup := GroupMemberModel.Api_select_byUid(uid, []interface{}{"owner", "admin"})
 	RET.Success(c, 0, usergroup, nil)
+}
+
+func group_setting_get(c *gin.Context) {
+	uid := c.PostForm("uid")
+	gid, ok := Input.PostInt64("gid", c)
+	if !ok {
+		return
+	}
+	if len(GroupMemberModel.Api_find(gid, uid)) > 0 {
+		group_setting := GroupFunctionModel.Api_find(gid)
+		if len(group_setting) < 1 {
+			RET.Fail(c, 404, nil, "本群不存在")
+		} else {
+			function := GroupFunctionDetailModel.Api_select_kv()
+			arr := make(map[string]map[string]interface{})
+			for k, v := range group_setting {
+				if function[k] != nil {
+					function[k]["value"] = v
+					arr[k] = function[k]
+				}
+			}
+			RET.Success(c, 0, arr, nil)
+		}
+	} else {
+		RET.Fail(c, 403, nil, "你不在这个群")
+	}
+
 }

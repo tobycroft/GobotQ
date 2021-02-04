92601f03f5c88204a163469193856f53face89eb
Tuuz <silverhawk@foxmail.com>
2021-02-04 22:28:41 +0800

    TuuzPC

--- app/bot/action/Private/UserLoginAction.go
+++ app/bot/action/Private/UserLoginAction.go
@@ -2,6 +2,7 @@ package Private
 
 import (
 	"main.go/app/bot/api"
+	"main.go/app/bot/model/GroupMemberModel"
 	"main.go/app/bot/model/UserMemberModel"
 	"main.go/config/app_default"
 	"main.go/tuuz/Calc"
@@ -17,17 +18,36 @@ func App_userLogin(bot int, uid int, text string) {
 		uname = nickname.Ret
 	}
 	usermember := UserMemberModel.Api_find(uid)
-	if len(usermember) > 0 {
-		if UserMemberModel.Api_update_all(uid, uname, rand) {
-			api.Sendprivatemsg(bot, uid, "您的登录密码：\r\n"+Calc.Int2String(rand), false)
+	groupmember := GroupMemberModel.Api_find_byUid(uid)
+	if len(groupmember) > 0 {
+		gid := groupmember["gid"]
+		if len(usermember) > 0 {
+			if UserMemberModel.Api_update_all(uid, uname, rand) {
+				api.Sendgrouptempmsg(bot, gid, uid, "您的登录密码：\r\n"+Calc.Int2String(rand))
+			} else {
+				api.Sendgrouptempmsg(bot, gid, uid, app_default.Default_error_alert)
+			}
 		} else {
-			api.Sendprivatemsg(bot, uid, app_default.Default_error_alert, false)
+			if UserMemberModel.Api_insert(uid, uname, rand) {
+				api.Sendgrouptempmsg(bot, gid, uid, "↓↓↓↓↓您的登录密码↓↓↓↓↓\r\n"+Calc.Int2String(rand)+"\r\n↑↑↑↑↑请在APP中输入↑↑↑↑↑\r\n"+app_default.Default_str_login_text)
+			} else {
+				api.Sendgrouptempmsg(bot, gid, uid, app_default.Default_error_alert)
+			}
 		}
 	} else {
-		if UserMemberModel.Api_insert(uid, uname, rand) {
-			api.Sendprivatemsg(bot, uid, "↓↓↓↓↓您的登录密码↓↓↓↓↓\r\n"+Calc.Int2String(rand)+"\r\n↑↑↑↑↑请在APP中输入↑↑↑↑↑\r\n"+app_default.Default_str_login_text, false)
+		if len(usermember) > 0 {
+			if UserMemberModel.Api_update_all(uid, uname, rand) {
+				api.Sendprivatemsg(bot, uid, "您的登录密码：\r\n"+Calc.Int2String(rand), false)
+			} else {
+				api.Sendprivatemsg(bot, uid, app_default.Default_error_alert, false)
+			}
 		} else {
-			api.Sendprivatemsg(bot, uid, app_default.Default_error_alert, false)
+			if UserMemberModel.Api_insert(uid, uname, rand) {
+				api.Sendprivatemsg(bot, uid, "↓↓↓↓↓您的登录密码↓↓↓↓↓\r\n"+Calc.Int2String(rand)+"\r\n↑↑↑↑↑请在APP中输入↑↑↑↑↑\r\n"+app_default.Default_str_login_text, false)
+			} else {
+				api.Sendprivatemsg(bot, uid, app_default.Default_error_alert, false)
+			}
 		}
 	}
+
 }

df312f8578d0a622958163308b0ca4bbc65a7782
Tuuz <silverhawk@foxmail.com>
2021-02-08 23:23:23 +0800

    TuuzPC

--- app/v1/index/controller/system.go
+++ app/v1/index/controller/system.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"main.go/app/v1/index/model/SystemAnnounceModel"
+	"main.go/app/v1/index/model/VersionModel"
+	"main.go/tuuz/Input"
 	"main.go/tuuz/RET"
 )
 
@@ -10,9 +12,23 @@ func SystemController(route *gin.RouterGroup) {
 	route.Any("/", index)
 
 	route.Any("announcement", announcement)
+	route.Any("version", version)
 }
 
 func announcement(c *gin.Context) {
 	data := SystemAnnounceModel.Api_select()
 	RET.Success(c, 0, data, nil)
+}
+
+func version(c *gin.Context) {
+	platform, ok := Input.Post("platform", c, false)
+	if !ok {
+		return
+	}
+	version := VersionModel.Api_find(platform)
+	if len(version) > 0 {
+		RET.Success(c, 0, version, nil)
+	} else {
+		RET.Fail(c, 404, nil, nil)
+	}
 }
---
+++ app/v1/index/model/VersionModel/VersionModel.go
@@ -0,0 +1,41 @@
+package VersionModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "version"
+
+func Api_find(platform interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"platform": platform,
+	}
+	db.Where(where)
+	db.Order("id desc")
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_select(platform interface{}) []gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"platform": platform,
+	}
+	db.Where(where)
+	db.Order("id desc")
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}

3bb733b96e5e76f3cc637446941ee3b5fe955733
Tuuz <silverhawk@foxmail.com>
2021-02-10 00:11:41 +0800

    TuuzPC

--- app/v1/bot/controller/bind.go
+++ app/v1/bot/controller/bind.go
@@ -1,7 +1,42 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"main.go/app/bot/model/BotModel"
+	"main.go/common/BaseController"
+	"main.go/tuuz/Calc"
+	"main.go/tuuz/Input"
+	"main.go/tuuz/RET"
+)
 
 func BindController(route *gin.RouterGroup) {
+	route.Use(BaseController.LoginedController(), gin.Recovery())
 
+	route.Any("add", bind_bot_add)
+}
+
+func bind_bot_add(c *gin.Context) {
+	uid := c.PostForm("uid")
+	bot, ok := Input.PostInt64("bot", c)
+	if !ok {
+		return
+	}
+	secret, ok := Input.Post("bot", c, false)
+	if !ok {
+		return
+	}
+	data := BotModel.Api_find(bot)
+	if len(data) > 0 {
+		if Calc.Any2String(data["secret"]) == secret {
+			if BotModel.Api_update_owner(bot, uid) {
+				RET.Success(c, 0, "绑定成功", "绑定成功")
+			} else {
+				RET.Fail(c, 500, "绑定失败", "绑定失败")
+			}
+		} else {
+			RET.Fail(c, 400, "密钥错误", "密钥错误")
+		}
+	} else {
+		RET.Fail(c, 404, "未找到这个机器人", "未找到这个机器人")
+	}
 }

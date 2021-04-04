616d94914dad1b8d9de9c4781c9b722c60cf3b6f
Tuuz <silverhawk@foxmail.com>
2021-04-04 16:48:11 +0800

    TuuzPC

--- app/bot/cron/AutoSend.go
+++ app/bot/cron/AutoSend.go
@@ -14,39 +14,43 @@ func Cron_auto_send() {
 	for _, data := range datas {
 		db := tuuz.Db()
 		db.Begin()
+		var ass AutoSendModel.Interface
+		ass.Db = db
 		switch data["type"].(string) {
 		case "sep":
 			timer := data["sep"].(int64)
 			next_time := time.Now().Unix() + timer
-			var ass AutoSendModel.Interface
-			ass.Db = db
+
 			ass.Api_update_next_time(data["gid"], data["id"], next_time)
-			var gss api.GroupSendStruct
-			if data["retract"].(int64) == 1 {
-				gss.AutoRetract = true
-			} else {
-				gss.AutoRetract = false
-			}
-			group := GroupListModel.Api_find(data["gid"])
-			if len(group) < 1 {
-				return
-			}
-			ass.Api_dec_count(data["id"])
-			db.Commit()
-			gss.Fromqq = group["bot"]
-			gss.Text = Calc.Any2String(data["msg"])
-			gss.Togroup = data["group"]
-			api.Group_send_chan <- gss
+
 			break
 
 		case "fix":
-
+			timer := data["sep"].(int64)
+			next_time := time.Now().Unix() + timer
+			ass.Api_update_next_time(data["gid"], data["id"], next_time)
+			ass.Api_update_active(data["gid"], data["id"], 0)
 			break
 
 		default:
 			break
 		}
-
+		var gss api.GroupSendStruct
+		if data["retract"].(int64) == 1 {
+			gss.AutoRetract = true
+		} else {
+			gss.AutoRetract = false
+		}
+		group := GroupListModel.Api_find(data["gid"])
+		if len(group) < 1 {
+			return
+		}
+		ass.Api_dec_count(data["id"])
+		db.Commit()
+		gss.Fromqq = group["bot"]
+		gss.Text = Calc.Any2String(data["msg"])
+		gss.Togroup = data["group"]
+		api.Group_send_chan <- gss
 	}
 }
 

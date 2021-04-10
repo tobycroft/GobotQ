f8c52f94bee0ccd8d75988142a18ee28a894ecb4
Tuuz <silverhawk@foxmail.com>
2021-04-10 23:51:25 +0800

    TuuzPC

--- app/bot/action/Group/user_operation.go
+++ app/bot/action/Group/user_operation.go
@@ -3,19 +3,48 @@ package Group
 import (
 	"main.go/app/bot/api"
 	"main.go/app/bot/model/GroupBanModel"
+	"main.go/app/bot/model/GroupMemberModel"
+	"main.go/app/bot/service"
+	"main.go/app/v1/user/action/BalanceAction"
 	"main.go/tuuz/Calc"
 	"math"
 )
 
-func App_ban_user(self_id, group_id, user_id interface{}, auto_retract bool, groupfunction map[string]interface{}) {
+func App_ban_user(self_id, group_id, user_id interface{}, auto_retract bool, groupfunction map[string]interface{}, reason string) {
+	at := service.Serv_at(user_id)
 	time := GroupBanModel.Api_count(group_id, user_id)
 	GroupBanModel.Api_insert(group_id, user_id)
-	api.Sendgroupmsg(self_id, group_id, "这是你第"+Calc.Any2String(time+1)+"次接受惩罚，积分扣除", auto_retract)
-	api.SetGroupBan(self_id, group_id, user_id, float64(groupfunction["ban_time"].(int64))*math.Pow10(int(time)))
+	left_time := groupfunction["ban_limit"].(int64) - 1
+	var balance BalanceAction.Interface
+	if left_time > 0 {
+		bal, _ := balance.App_check_balance(user_id)
+		balance_decr := float64(time) * math.Pow10(int(time))
+		balance_left := bal - balance_decr
+		if balance_left < 0 {
+			api.Sendgroupmsg(self_id, group_id, at+"这是你第:"+Calc.Any2String(time+1)+"次，接受惩罚\n"+"本次惩罚原因："+reason+"\n你还剩下："+Calc.Any2String(left_time)+"点生命值", auto_retract)
+			api.SetGroupBan(self_id, group_id, user_id, float64(groupfunction["ban_time"].(int64))*math.Pow10(int(time)))
+		} else {
+			BalanceAction.App_single_balance(user_id, "", -math.Abs(balance_decr), "扣分")
+			api.Sendgroupmsg(self_id, group_id, at+"这是你第:"+Calc.Any2String(time+1)+"次扣分\n"+"本次扣分原因："+reason+"\n你还剩下："+Calc.Any2String(balance_left)+"分", auto_retract)
+		}
+	} else {
+		App_kick_user(self_id, group_id, user_id, auto_retract, groupfunction, reason+"\n且他已经没有生命值了")
+	}
 }
 
-func Api_kick_user(bot, gid, uid int, req int, random int, groupmember map[string]interface{}, groupfunction map[string]interface{}) {
+func App_kick_user(self_id, group_id, user_id interface{}, auto_retract bool, groupfunction map[string]interface{}, reason string) {
+	auto_kick_out := groupfunction["auto_kick_out"].(int64)
+	if auto_kick_out == 1 {
+		gm := GroupMemberModel.Api_find(group_id, user_id)
+		if len(gm) > 0 {
+			nickname := Calc.Any2String(gm["nickname"])
+			api.Sendgroupmsg(self_id, group_id, nickname+"被T出，原因为："+reason, auto_retract)
+		} else {
+			api.Sendgroupmsg(self_id, group_id, Calc.Any2String(user_id)+"被T出，原因为："+reason, auto_retract)
+		}
+	} else {
 
+	}
 }
 
 func Api_retract_send(bot, gid, uid int, req int, random int, groupmember map[string]interface{}, groupfunction map[string]interface{}) {
---
+++ app/bot/model/GroupBanPermenentModel/GroupBanPermenentModel.go
@@ -0,0 +1,88 @@
+package GroupBanModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "group_ban_permenent"
+
+func Api_insert(group_id, user_id interface{}) bool {
+	db := tuuz.Db().Table(table)
+	data := map[string]interface{}{
+		"group_id": group_id,
+		"user_id":  user_id,
+	}
+	db.Data(data)
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_find(group_id, user_id interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"group_id": group_id,
+		"user_id":  user_id,
+	}
+	db.Where(where)
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_count(group_id, user_id interface{}) int64 {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"group_id": group_id,
+		"user_id":  user_id,
+	}
+	db.Where(where)
+	ret, err := db.Count()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return 0
+	} else {
+		return ret
+	}
+}
+
+func Api_select(group_id, user_id interface{}) []gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"group_id": group_id,
+		"user_id":  user_id,
+	}
+	db.Where(where)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_delete(group_id interface{}) int64 {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"group_id": group_id,
+	}
+	db.Where(where)
+	ret, err := db.Count()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return 0
+	} else {
+		return ret
+	}
+}

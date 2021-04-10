3fea38010961a746751baa66d3dee6d362fee357
Tuuz <silverhawk@foxmail.com>
2021-04-10 19:56:43 +0800

    TuuzPC

--- app/bot/service/at.go
+++ app/bot/service/at.go
@@ -1,6 +1,9 @@
 package service
 
-import "main.go/tuuz/Calc"
+import (
+	"main.go/tuuz/Calc"
+	"strings"
+)
 
 func Serv_at(qq interface{}) string {
 	return "[CQ:at,qq=" + Calc.Any2String(qq) + "]"
@@ -8,4 +11,8 @@ func Serv_at(qq interface{}) string {
 
 func Serv_at_all() string {
 	return "[CQ:at,qq=all]"
+}
+
+func Serv_is_at_me(self_id interface{}, message string) bool {
+	return strings.Contains(message, Serv_at(self_id))
 }
---
+++ app/bot/service/cq_handler.go
@@ -0,0 +1,5 @@
+package service
+
+func CQ_Handler(message string) {
+
+}

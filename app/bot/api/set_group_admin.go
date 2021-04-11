ef4a4b8b78cec5b27b81e32dc057bfb466af6464
Tuuz <silverhawk@foxmail.com>
2021-04-11 13:45:17 +0800

    TuuzPC

---
+++ app/bot/api/set_group_admin.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"errors"
+	jsoniter "github.com/json-iterator/go"
+	"main.go/app/bot/model/BotModel"
+	"main.go/tuuz"
+	"main.go/tuuz/Calc"
+	"main.go/tuuz/Log"
+	"main.go/tuuz/Net"
+)
+
+func SetGroupAdmin(self_id, group_id, user_id interface{}, enable bool) (bool, error) {
+	post := map[string]interface{}{
+		"group_id": group_id,
+		"user_id":  user_id,
+		"enable":   enable,
+	}
+	botinfo := BotModel.Api_find(self_id)
+	if len(botinfo) < 1 {
+		Log.Crrs(nil, "bot:"+Calc.Any2String(self_id))
+		return false, errors.New("botinfo_notfound")
+	}
+	data, err := Net.Post(botinfo["url"].(string)+"/set_group_admin", nil, post, nil, nil)
+	if err != nil {
+		return false, err
+	}
+	var dls DefaultRetStruct
+	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	err = jsr.UnmarshalFromString(data, &dls)
+	if err != nil {
+		return false, err
+	}
+	if dls.Retcode == 0 {
+		return true, nil
+	} else {
+		Log.Crrs(errors.New(dls.Wording), tuuz.FUNCTION_ALL())
+		return false, errors.New(dls.Wording)
+	}
+}

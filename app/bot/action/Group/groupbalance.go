95cc4f16a87aa5042ab173b6bc0e6c1bd7717345
Tuuz <silverhawk@foxmail.com>
2021-04-27 21:37:03 +0800

    TuuzPC

--- app/bot/action/Group/groupbalance.go
+++ app/bot/action/Group/groupbalance.go
@@ -1,35 +1,33 @@
 package Group
 
 import (
-	"main.go/app/bot/api"
 	"main.go/app/bot/model/GroupBalanceModel"
 	"main.go/app/bot/model/GroupMemberModel"
 	"main.go/tuuz/Calc"
 )
 
 func App_check_balance(self_id, group_id, user_id, message_id int64, groupmember map[string]interface{}, groupfunction map[string]interface{}) {
-	auto_retract := false
-	if groupfunction["sign_send_retract"].(int64) == 1 {
-		auto_retract = true
-		var ret api.Struct_Retract
-		ret.MessageId = message_id
-		ret.Self_id = self_id
-		api.Retract_chan <- ret
-	}
+	//auto_retract := false
+	//if groupfunction["sign_send_retract"].(int64) == 1 {
+	//	auto_retract = true
+	//	var ret api.Struct_Retract
+	//	ret.MessageId = message_id
+	//	ret.Self_id = self_id
+	//	api.Retract_chan <- ret
+	//}
 	gbl := GroupBalanceModel.Api_find(group_id, user_id)
 	str := "您当前拥有" + Calc.Any2String(gbl["balance"]) + "分"
-	api.Sendgroupmsg(self_id, group_id, str, auto_retract)
+	AutoMessage(self_id, group_id, user_id, str, groupfunction)
 }
 
 func App_check_rank(self_id, group_id, user_id, message_id int64, groupmember map[string]interface{}, groupfunction map[string]interface{}) {
-	auto_retract := false
-	if groupfunction["sign_send_retract"].(int64) == 1 {
-		auto_retract = true
-		var ret api.Struct_Retract
-		ret.MessageId = message_id
-		ret.Self_id = self_id
-		api.Retract_chan <- ret
-	}
+	//auto_retract := false
+	//if groupfunction["sign_send_retract"].(int64) == 1 {
+	//	var ret api.Struct_Retract
+	//	ret.MessageId = message_id
+	//	ret.Self_id = self_id
+	//	api.Retract_chan <- ret
+	//}
 	gbl := GroupBalanceModel.Api_select(group_id, 10)
 	str := ""
 	for i1, i2 := range gbl {
@@ -42,5 +40,5 @@ func App_check_rank(self_id, group_id, user_id, message_id int64, groupmember ma
 			}
 		}
 	}
-	api.Sendgroupmsg(self_id, group_id, str, auto_retract)
+	AutoMessage(self_id, group_id, user_id, str, groupfunction)
 }

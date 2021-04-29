92f20341e751117a27d70eeef551ffcae20b3fd1
Tuuz <miracle-plus@outlook.com>
2021-04-29 17:27:15 +0800

    TuuzMac

--- app/v1/bot/controller/server.go
+++ app/v1/bot/controller/server.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"main.go/common/BaseController"
+	"main.go/tuuz/Input"
+	"main.go/tuuz/Net"
 )
 
 func ServerController(route *gin.RouterGroup) {
@@ -12,7 +14,17 @@ func ServerController(route *gin.RouterGroup) {
 }
 
 func add(c *gin.Context) {
+	address, ok := Input.Post("address", c, false)
+	if !ok {
+		return
+	}
 
+	ret, err := Net.Post("docker.tuuz.cc:5701/get_status", nil, nil, nil, nil)
+	if err != nil {
+
+	} else {
+
+	}
 }
 
 func update(c *gin.Context) {

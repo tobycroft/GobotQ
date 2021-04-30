567d17df876ada9cdda4d2e0842b07155b1ff6aa
Tuuz <miracle-plus@outlook.com>
2021-04-30 11:48:50 +0800

    TuuzMac

--- main.go
+++ main.go
@@ -1,38 +1,31 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"main.go/app/bot/action/Group"
-	"main.go/app/bot/cron"
-	"main.go/route"
-)
-
 func main() {
 
-	go cron.BaseCron()
-	go cron.Refresh_friend_list()
-
-	go Group.App_refresh_group_member_chan()
-
-	go cron.Refresh_group_chan()
-
-	go cron.Retract()
-	go cron.Send_private()
-	go cron.Send_group()
-
-	go cron.GroupMsgRecv()
-	go cron.PrivateMsgRecv()
-
-	go cron.Cron_auto_send()
-
-	go cron.BanPermenentCheck()
-
-	go cron.PowerCheck()
-
-	mainroute := gin.Default()
-	//gin.SetMode(gin.ReleaseMode)
-	//gin.DefaultWriter = ioutil.Discard
-	route.OnRoute(mainroute)
-	mainroute.Run(":81")
+	//go cron.BaseCron()
+	//go cron.Refresh_friend_list()
+	//
+	//go Group.App_refresh_group_member_chan()
+	//
+	//go cron.Refresh_group_chan()
+	//
+	//go cron.Retract()
+	//go cron.Send_private()
+	//go cron.Send_group()
+	//
+	//go cron.GroupMsgRecv()
+	//go cron.PrivateMsgRecv()
+	//
+	//go cron.Cron_auto_send()
+	//
+	//go cron.BanPermenentCheck()
+	//
+	//go cron.PowerCheck()
+	//
+	//mainroute := gin.Default()
+	////gin.SetMode(gin.ReleaseMode)
+	////gin.DefaultWriter = ioutil.Discard
+	//route.OnRoute(mainroute)
+	//mainroute.Run(":81")
 
 }

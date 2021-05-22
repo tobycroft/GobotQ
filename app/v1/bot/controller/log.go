5bcdc71921335d35bc96003ce22746d5f8c151d1
Tuuz <miracle-plus@outlook.com>
2021-05-22 21:41:37 +0800

    TuuzMac

--- app/v1/bot/controller/log.go
+++ app/v1/bot/controller/log.go
@@ -3,22 +3,33 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"main.go/common/BaseController"
+	"main.go/tuuz/Input"
 )
 
 func LogController(route *gin.RouterGroup) {
 	route.Use(BaseController.LoginedController(), gin.Recovery())
 
-	route.Any("common", log_common_list)
+	route.Any("common", log_list_common)
+	route.Any("all", log_list_all)
+	route.Any("get", log_get)
 }
 
-func log_common_list(c *gin.Context) {
+func log_list_common(c *gin.Context) {
+	page, ok := Input.PostInt("page", c)
+	if !ok {
+		return
+	}
+	limit, ok := Input.PostInt("limit", c)
+	if !ok {
+		return
+	}
 
 }
 
-func log_all(c *gin.Context) {
+func log_list_all(c *gin.Context) {
 
 }
 
-func log_list(c *gin.Context) {
+func log_get(c *gin.Context) {
 
 }
---
+++ app/v1/bot/model/BotLogModel/BotLogModel.go
@@ -0,0 +1,67 @@
+package BotLogModel
+
+import (
+	"github.com/gohouse/gorose/v2"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const table = "bot_log"
+
+/*
+table:bot_log
+
+collection:
+
+self_id
+log
+type
+date
+*/
+
+func Api_insert(self_id, log interface{}) bool {
+	db := tuuz.Db().Table(table)
+	db.Data(map[string]interface{}{
+		"log":     log,
+		"self_id": self_id,
+	})
+	_, err := db.Insert()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+
+}
+
+func Api_select(self_id interface{}, page, limit int) []gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"self_id": self_id,
+	}
+	db.Where(where)
+	ret, err := db.Get()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}
+
+func Api_get(self_id, id interface{}) gorose.Data {
+	db := tuuz.Db().Table(table)
+	where := map[string]interface{}{
+		"self_id": self_id,
+		"id":      id,
+	}
+	db.Where(where)
+	ret, err := db.First()
+	if err != nil {
+		Log.Dbrr(err, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}

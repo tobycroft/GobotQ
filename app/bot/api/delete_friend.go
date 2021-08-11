50ad7d86c00a8ff867f5384c788042d05e9ba3c6
火线兔 <silverhawk@foxmail.com>
2021-08-11 15:15:39 +0800

    TuuzPC

---
+++ app/bot/api/delete_friend.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"errors"
+	jsoniter "github.com/json-iterator/go"
+	"main.go/app/bot/model/BotModel"
+	"main.go/tuuz/Calc"
+	"main.go/tuuz/Log"
+	"main.go/tuuz/Net"
+)
+
+func DeleteFriend(self_id, friend_id interface{}) (bool, error) {
+	post := map[string]interface{}{
+		"friend_id": friend_id,
+	}
+	botinfo := BotModel.Api_find(self_id)
+	if len(botinfo) < 1 {
+		Log.Crrs(nil, "bot:"+Calc.Any2String(self_id))
+		return false, errors.New("botinfo_notfound")
+	}
+	data, err := Net.Post(botinfo["url"].(string)+"/delete_msg", nil, post, nil, nil)
+	if err != nil {
+		return false, err
+	}
+	var dls DefaultRetStruct
+	jsr := jsoniter.ConfigCompatibleWithStandardLibrary
+	err = jsr.UnmarshalFromString(data, &dls)
+	if err != nil {
+		return false, err
+	}
+	if dls.Retcode == 0 {
+		return true, nil
+	} else {
+		Log.Crrs(errors.New(dls.Wording), "message:"+Calc.Any2String(friend_id))
+		return false, errors.New(dls.Wording)
+	}
+}

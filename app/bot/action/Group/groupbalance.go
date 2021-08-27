8a040801c30747632f838f1c0dcc329933cb47e1
火线兔 <silverhawk@foxmail.com>
2021-08-28 00:29:33 +0800

    TuuzPC

--- app/bot/action/Group/groupbalance.go
+++ app/bot/action/Group/groupbalance.go
@@ -4,6 +4,7 @@ import (
 	"main.go/app/bot/api"
 	"main.go/app/bot/model/GroupBalanceModel"
 	"main.go/app/bot/model/GroupMemberModel"
+	"main.go/app/bot/service"
 	"main.go/tuuz/Calc"
 )
 
@@ -17,7 +18,8 @@ func App_check_balance(self_id, group_id, user_id, message_id int64, groupmember
 		api.Retract_chan <- ret
 	}
 	gbl := GroupBalanceModel.Api_find(group_id, user_id)
-	str := "您当前拥有" + Calc.Any2String(gbl["balance"]) + "分"
+	at:=service.Serv_at(user_id)
+	str := at+"您当前拥有" + Calc.Any2String(gbl["balance"]) + "分"
 	api.Sendgroupmsg(self_id, group_id, str, auto_retract)
 }
 

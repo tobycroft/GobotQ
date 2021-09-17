cdc66cde81f2b2dd5152bd4c28de091f42635508
火线兔 <silverhawk@foxmail.com>
2021-09-17 18:11:40 +0800

    TuuzPC

--- app/bot/action/Group/auto_message.go
+++ app/bot/action/Group/auto_message.go
@@ -8,8 +8,8 @@ func AutoMessage(self_id, group_id, user_id interface{}, message string, groupfu
 		AutoRetract = true
 	}
 	if groupfunction["all_send_private"].(int64) == 1 {
-		api.Sendprivatemsg(self_id, user_id, group_id, message, AutoRetract)
+		go api.Sendprivatemsg(self_id, user_id, group_id, message, AutoRetract)
 	} else {
-		api.Sendgroupmsg(self_id, group_id, message, AutoRetract)
+		go api.Sendgroupmsg(self_id, group_id, message, AutoRetract)
 	}
 }

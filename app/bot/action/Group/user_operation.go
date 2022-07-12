e25369632a924ebc242ae070a7d94799164f05f1
火线兔 <silverhawk@foxmail.com>
2022-07-12 23:13:09 +0800

    TuuzMac

--- app/bot/action/Group/user_operation.go
+++ app/bot/action/Group/user_operation.go
@@ -125,7 +125,7 @@ func App_drcrease_member(self_id, group_id, user_id interface{}, groupfunction m
 					"\n最后一个被清除的为:"+Calc.Any2String(group_member_datas[len(group_member_datas)-1]["nickname"])+
 					"，他最后一次说话是在："+Date.Date_format_second(group_member_datas[len(group_member_datas)-1]["last_date"].(time.Time)), false)
 				for _, data := range group_member_datas {
-					api.SetGroupKick(self_id, group_id, data["user_id"], false)
+					go api.SetGroupKick(self_id, group_id, data["user_id"], false)
 				}
 			} else {
 				api.Sendgroupmsg(self_id, group_id, "没有需要清理的人", true)

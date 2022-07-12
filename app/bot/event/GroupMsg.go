7c9cfa69a57364a6b6a587171560ac4a5074bb77
火线兔 <silverhawk@foxmail.com>
2022-07-12 23:33:15 +0800

    TuuzMac

--- app/bot/event/GroupMsg.go
+++ app/bot/event/GroupMsg.go
@@ -327,7 +327,7 @@ func groupHandle_acfur(self_id, group_id, user_id int64, message_id int64, new_t
 			Group.AutoMessage(self_id, group_id, user_id, "执行中请稍等", groupfunction)
 		} else {
 			Redis.Set_add("__lock__group_id__"+Calc.Any2String(group_id), 1, 60)
-			Group.App_drcrease_member(self_id, group_id, user_id, groupfunction, "清理人数上限")
+			Group.App_drcrease_member(self_id, group_id, user_id, groupfunction, "")
 		}
 		break
 

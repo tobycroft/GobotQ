f64916bd27de92c5d6bc9f406dbcf5019d8cf92e
火线兔 <silverhawk@foxmail.com>
2022-07-13 13:38:55 +0800

    TuuzMac

--- tuuz/Redis/goredis.go
+++ tuuz/Redis/goredis.go
@@ -3,6 +3,7 @@ package Redis
 import (
 	"context"
 	"github.com/go-redis/redis/v9"
+	"log"
 	"main.go/config/app_conf"
 	"time"
 )
@@ -20,10 +21,6 @@ func init() {
 		Username:     app_conf.Redicon_username,
 		Password:     app_conf.Redicon_password, // no password set
 		Network:      app_conf.Redicon_proto,
-		DialTimeout:  app_conf.Redicon_timeout,
-		PoolTimeout:  30,
-		ReadTimeout:  app_conf.Redicon_readtimeout,
-		WriteTimeout: app_conf.Redicon_writetimeout,
 		DB:           0, // use default DB
 		MinIdleConns: app_conf.Redicon_MinIdleConn,
 	}
@@ -34,9 +31,9 @@ func init() {
 	go func() {
 		for {
 			time.Sleep(3 * time.Second)
-			_, err := goredis.Ping(goredis_ctx).Result()
+			ret, err := goredis.Ping(goredis_ctx).Result()
 			if err != nil {
-				panic("redis_out")
+				log.Println("redis_out", ret, err)
 			}
 		}
 	}()

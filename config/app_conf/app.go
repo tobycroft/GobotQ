ee943753f6d721224035ede6e180ba7509438f57
tuuz <silverhawk@foxmail.com>
2023-11-14 16:02:47 +0800

    使用了插件方法方便未来更新Websocket

--- config/app_conf/app.go
+++ config/app_conf/app.go
@@ -1,6 +1,6 @@
 package app_conf
 
-const Project = "gobot"
+const Project = "gobotq"
 
 const Debug = "65535"
 const TestMode = true
@@ -9,10 +9,10 @@ const TestMode = true
 
 const Retract_time_second = 100
 
-//每日签到威望
+// 每日签到威望
 const Group_Sign_incr = 10
 
 const Qq = 710209520
 
-//设定永久小黑屋禁言时间
+// 设定永久小黑屋禁言时间
 const Auto_ban_time = 2419200

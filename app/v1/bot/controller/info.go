3c936c7b7de69e17444fb0d2cb589deb87f9ae07
tuuz <silverhawk@foxmail.com>
2023-11-16 23:19:57 +0800

    使用了插件方法方便未来更新Websocket

--- app/v1/bot/controller/info.go
+++ app/v1/bot/controller/info.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/tobycroft/Calc"
 	"main.go/app/bot/model/BotModel"
 	"main.go/common/BaseController"
 	"main.go/tuuz/Input"
@@ -10,6 +11,7 @@ import (
 
 func InfoController(route *gin.RouterGroup) {
 	route.Use(BaseController.LoginedController(), gin.Recovery())
+	route.Any("bind", info_bind)
 	route.Any("get", info_get)
 	route.Any("unbind", info_unbind)
 	route.Any("owned", info_yours_own)
@@ -37,5 +39,35 @@ func info_get(c *gin.Context) {
 		RET.Success(c, 0, botinfo, nil)
 	} else {
 		RET.Fail(c, 404, nil, nil)
+	}
+}
+
+func info_bind(c *gin.Context) {
+	uid := c.GetHeader("uid")
+	bot, ok := Input.PostInt64("self_id", c)
+	if !ok {
+		return
+	}
+	secret, ok := Input.Post("secret", c, false)
+	if !ok {
+		return
+	}
+	data := BotModel.Api_find(bot)
+	if len(data) > 0 {
+		if Calc.Any2Int64(data["owner"]) != 0 {
+			RET.Fail(c, 407, "该机器人已经被绑定", "该机器人已经被绑定")
+			return
+		}
+		if Calc.Any2String(data["secret"]) == secret {
+			if BotModel.Api_update_owner(bot, uid) {
+				RET.Success(c, 0, "绑定成功", "绑定成功")
+			} else {
+				RET.Fail(c, 500, "绑定失败", "绑定失败")
+			}
+		} else {
+			RET.Fail(c, 400, "密钥错误", "密钥错误")
+		}
+	} else {
+		RET.Fail(c, 404, "未找到这个机器人", "未找到这个机器人")
 	}
 }

3c090eb0b01c68af4384a7e7b58720ed046df399
tuuz <silverhawk@foxmail.com>
2023-11-25 13:12:19 +0800

    使用了插件方法方便未来更新Websocket

---
+++ app/bot/model/BotSettingModel/BotSettingModel.go
@@ -0,0 +1,37 @@
+package BotSettingModel
+
+import (
+	"github.com/tobycroft/gorose-pro"
+	"main.go/tuuz"
+	"main.go/tuuz/Log"
+)
+
+const Table = "bot_setting"
+
+func Api_insert(self_id, add_friend, add_group any) bool {
+	db := tuuz.Db().Table(Table)
+	db.Data(map[string]any{
+		"self_id":    self_id,
+		"add_friend": add_friend,
+		"add_group":  add_group,
+	})
+	_, err := db.Insert()
+	if err != nil {
+		Log.DBrrsql(err, db, tuuz.FUNCTION_ALL())
+		return false
+	} else {
+		return true
+	}
+}
+
+func Api_find(self_id any) gorose.Data {
+	db := tuuz.Db().Table(Table)
+	db.Where("self_id", self_id)
+	ret, err := db.Find()
+	if err != nil {
+		Log.DBrrsql(err, db, tuuz.FUNCTION_ALL())
+		return nil
+	} else {
+		return ret
+	}
+}

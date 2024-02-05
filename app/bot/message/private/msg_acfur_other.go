a252a954b6c7de8526fcb4535b4a1d595437ca56
tuuz <silverhawk@foxmail.com>
2024-02-06 05:47:49 +0800

    使用RedisPubsub队列解耦

--- app/bot/message/private/msg_acfur_other.go
+++ app/bot/message/private/msg_acfur_other.go
@@ -10,7 +10,6 @@ import (
 	"main.go/config/types"
 	"main.go/tuuz/Redis"
 	"regexp"
-	"sync"
 )
 
 const private_function_number = 5
@@ -26,7 +25,7 @@ func private_message_setting_change_with_acfur() {
 			fmt.Println(err)
 		} else {
 			pm := es.Json
-			self_id := pm.SelfId
+			selfId := pm.SelfId
 			user_id := pm.UserId
 			group_id := int64(0)
 			message := pm.RawMessage
@@ -35,105 +34,37 @@ func private_message_setting_change_with_acfur() {
 			active := reg.MatchString(message)
 			new_text := reg.ReplaceAllString(message, "")
 			if active {
-
+				botinfo := BotModel.Api_find(selfId)
+				if msg, ok := service.Serv_text_match(new_text, []string{"密码"}); ok {
+					if int64(user_id) == botinfo["owner"].(int64) {
+						Private.App_userChangePassword(selfId, user_id, group_id, msg)
+					} else {
+						iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "您未拥有这个机器人的权限，请先绑定机器人", true)
+					}
+				} else if msg, ok := service.Serv_text_match(new_text, []string{"绑定"}); ok {
+					Private.App_bind_robot(selfId, user_id, group_id, msg)
+				} else if msg, ok := service.Serv_text_match(new_text, []string{"修改密码"}); ok {
+					if int64(user_id) == botinfo["owner"].(int64) {
+						Private.App_change_bot_secret(selfId, user_id, group_id, msg)
+					} else {
+						iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "您未拥有这个机器人的权限，请先绑定机器人", true)
+					}
+				} else if msg, ok := service.Serv_text_match(new_text, []string{"绑定群"}); ok {
+					if int64(user_id) == botinfo["owner"].(int64) {
+						Private.App_bind_group(selfId, user_id, group_id, msg)
+					} else {
+						iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "您未拥有这个机器人的权限，请先绑定机器人", true)
+					}
+				} else if msg, ok := service.Serv_text_match(new_text, []string{"解绑群"}); ok {
+					if int64(user_id) == botinfo["owner"].(int64) {
+						Private.App_unbind_group(selfId, user_id, group_id, msg)
+					} else {
+						iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "您未拥有这个机器人的权限，请先绑定机器人", true)
+					}
+				} else {
+					iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "Hi我是Acfur！如果需要帮助请发送acfurhelp", false)
+				}
 			}
 		}
-	}
-}
-
-func acfur_password() {
-
-	service.Serv_text_match(message, []string{"密码", "password"})
-}
-
-func privateHandle_acfur_middle(selfId, user_id, group_id int64, message, origin_text string) {
-	function := make([]bool, private_function_number+1, private_function_number+1)
-	new_text := make([]string, private_function_number+1, private_function_number+1)
-	var wg sync.WaitGroup
-	wg.Add(private_function_number)
-
-	go func(idx int64, wg *sync.WaitGroup) {
-		defer wg.Done()
-		str, ok := service.Serv_text_match(message, []string{"密码", "password"})
-		new_text[idx] = str
-		function[idx] = ok
-	}(1, &wg)
-	go func(idx int64, wg *sync.WaitGroup) {
-		defer wg.Done()
-		str, ok := service.Serv_text_match(message, []string{"修改密码", "change_secret"})
-		new_text[idx] = str
-		function[idx] = ok
-	}(2, &wg)
-	go func(idx int64, wg *sync.WaitGroup) {
-		defer wg.Done()
-		str, ok := service.Serv_text_match(message, []string{"绑定群", "bindgroup"})
-		new_text[idx] = str
-		function[idx] = ok
-	}(3, &wg)
-	go func(idx int64, wg *sync.WaitGroup) {
-		defer wg.Done()
-		str, ok := service.Serv_text_match(message, []string{"解绑群", "unbindgroup"})
-		new_text[idx] = str
-		function[idx] = ok
-	}(4, &wg)
-	go func(idx int64, wg *sync.WaitGroup) {
-		defer wg.Done()
-		str, ok := service.Serv_text_match(message, []string{"绑定", "bind"})
-		new_text[idx] = str
-		function[idx] = ok
-	}(5, &wg)
-	wg.Wait()
-	function_route := 0
-	for i := range function {
-		if function[i] == true {
-			function_route = i
-			break
-		}
-	}
-	privateHandle_acfur_other(private_function_type[function_route], selfId, user_id, group_id, new_text[function_route])
-}
-
-func privateHandle_acfur_other(Type string, selfId int64, user_id, group_id int64, message string) {
-	botinfo := BotModel.Api_find(selfId)
-	switch Type {
-	case "密码":
-		if int64(user_id) == botinfo["owner"].(int64) {
-			Private.App_userChangePassword(selfId, user_id, group_id, message)
-		} else {
-			iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "您未拥有这个机器人的权限，请先绑定机器人", true)
-		}
-		break
-
-	case "绑定":
-		Private.App_bind_robot(selfId, user_id, group_id, message)
-		break
-
-	case "修改密码":
-		if int64(user_id) == botinfo["owner"].(int64) {
-			Private.App_change_bot_secret(selfId, user_id, group_id, message)
-		} else {
-			iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "您未拥有这个机器人的权限，请先绑定机器人", true)
-		}
-		break
-
-	case "绑定群":
-		if int64(user_id) == botinfo["owner"].(int64) {
-			Private.App_bind_group(selfId, user_id, group_id, message)
-		} else {
-			iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "您未拥有这个机器人的权限，请先绑定机器人", true)
-		}
-		break
-
-	case "解绑群":
-		if int64(user_id) == botinfo["owner"].(int64) {
-			Private.App_unbind_group(selfId, user_id, group_id, message)
-		} else {
-			iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "您未拥有这个机器人的权限，请先绑定机器人", true)
-		}
-		break
-
-	default:
-		iapi.Api.Sendprivatemsg(selfId, user_id, group_id, "Hi我是Acfur！如果需要帮助请发送acfurhelp", false)
-		break
 	}
 }

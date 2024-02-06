97af3f1c1fd8a9c4f40b746e39140c7dda840e08
tuuz <silverhawk@foxmail.com>
2024-02-07 01:10:37 +0800

    使用RedisPubsub队列解耦

--- app/bot/message/operation/router.go
+++ app/bot/message/operation/router.go
@@ -42,82 +42,88 @@ func Router() {
 			self_id := oe.Echo.SelfId
 			switch oe.Echo.Action {
 			case "get_login_info":
-				logininfo := iapi.LoginInfoRet{}
-				err := sonic.UnmarshalString(es.Json, &logininfo)
+				var es EventStruct[iapi.LoginInfoRet]
+				err := sonic.UnmarshalString(c.Payload, &es)
 				if err != nil {
-					fmt.Println(oe.json)
-					break
-				}
-				user_id := logininfo.Data.UserID
-				nickname := logininfo.Data.Nickname
-				if !BotModel.Api_update_cname(user_id, nickname) {
-					Log.Crrs(errors.New("机器人用户名无法更新"), tuuz.FUNCTION_ALL())
+					Log.Errs(err, tuuz.FUNCTION_ALL())
 				} else {
-					fmt.Println("机器人更新完毕：", logininfo.Data)
+					logininfo := es.Json
+					user_id := logininfo.Data.UserID
+					nickname := logininfo.Data.Nickname
+					if !BotModel.Api_update_cname(user_id, nickname) {
+						Log.Crrs(errors.New("机器人用户名无法更新"), tuuz.FUNCTION_ALL())
+					} else {
+						fmt.Println("机器人更新完毕：", logininfo.Data)
+					}
+					if len(BotSettingModel.Api_find(self_id)) < 1 {
+						BotSettingModel.Api_insert(self_id, 0, 0)
+					}
+					iapi.Api.Getfriendlist(self_id)
 				}
-				if len(BotSettingModel.Api_find(self_id)) < 1 {
-					BotSettingModel.Api_insert(self_id, 0, 0)
-				}
-				iapi.Api.Getfriendlist(self_id)
+
 				break
 
 			case "get_friend_list":
-				data := iapi.FriendListRet{}
-				err := sonic.UnmarshalString(oe.json, &data)
+				var es EventStruct[iapi.FriendListRet]
+				err := sonic.UnmarshalString(c.Payload, &es)
 				if err != nil {
-					fmt.Println(err, oe.json)
-					break
+					Log.Errs(err, tuuz.FUNCTION_ALL())
+				} else {
+					data := es.Json
+					Private.App_refresh_friend_list_action(self_id, data.Data)
+					fmt.Println("好友列表更新完毕：", oe.Echo.SelfId)
+					iapi.Api.Getgrouplist(self_id)
 				}
-				Private.App_refresh_friend_list_action(self_id, data.Data)
-				fmt.Println("好友列表更新完毕：", oe.Echo.SelfId)
-				iapi.Api.Getgrouplist(self_id)
 				break
 
 			case "get_group_list":
-				data := iapi.GroupListRet{}
-				err := sonic.UnmarshalString(oe.json, &data)
+				var es EventStruct[iapi.GroupListRet]
+				err := sonic.UnmarshalString(c.Payload, &es)
 				if err != nil {
-					fmt.Println(err, oe.json)
-					break
-				}
-				Group.App_refresh_group_list_action(self_id, data.Data)
-				fmt.Println("群列表更新完毕：", oe.Echo.SelfId)
-				for _, datum := range data.Data {
-					num := GroupMemberModel.Api_count_byGroupIdAndRole(datum.GroupId, nil)
-					if num-datum.MemberNum != 0 {
-						log.Println("需要更新的群：", self_id, datum.GroupId, num, datum.MemberNum, datum.MemberCount)
-						Group.Chan_refresh_group_member <- Group.App_group_member{
-							SelfId: self_id, GroupId: datum.GroupId,
+					Log.Errs(err, tuuz.FUNCTION_ALL())
+				} else {
+					data := es.Json
+					Group.App_refresh_group_list_action(self_id, data.Data)
+					fmt.Println("群列表更新完毕：", oe.Echo.SelfId)
+					for _, datum := range data.Data {
+						num := GroupMemberModel.Api_count_byGroupIdAndRole(datum.GroupId, nil)
+						if num-datum.MemberNum != 0 {
+							log.Println("需要更新的群：", self_id, datum.GroupId, num, datum.MemberNum, datum.MemberCount)
+							Group.Chan_refresh_group_member <- Group.App_group_member{
+								SelfId: self_id, GroupId: datum.GroupId,
+							}
 						}
 					}
 				}
 				break
 
 			case "get_group_member_list":
-				data := iapi.GroupMemberListRet{}
-				err := sonic.UnmarshalString(oe.json, &data)
+				var es EventStruct[iapi.GroupMemberListRet]
+				err := sonic.UnmarshalString(c.Payload, &es)
 				if err != nil {
-					fmt.Println(err, oe.json)
-					break
+					Log.Errs(err, tuuz.FUNCTION_ALL())
+				} else {
+					data := es.Json
+					Group.App_refresh_group_member_one_action(self_id, data.Data)
+					fmt.Println("群成员更新完毕：", oe.Echo.SelfId, Calc.Any2Int64(oe.Echo.Extra))
 				}
-				Group.App_refresh_group_member_one_action(self_id, data.Data)
-				fmt.Println("群成员更新完毕：", oe.Echo.SelfId, Calc.Any2Int64(oe.Echo.Extra))
 				break
 
 			case "send_private_msg", "send_group_msg":
 				if oe.Echo.Extra.(bool) {
-					data := iapi.MessageRet{}
-					err := sonic.UnmarshalString(oe.json, &data)
+					var es EventStruct[iapi.MessageRet]
+					err := sonic.UnmarshalString(c.Payload, &es)
 					if err != nil {
-						fmt.Println(err, oe.json)
-						break
+						Log.Errs(err, tuuz.FUNCTION_ALL())
+					} else {
+						data := es.Json
+						var rm iapi.RetractMessage
+						rm.MessageId = data.Data.MessageId
+						rm.SelfId = oe.Echo.SelfId
+						rm.Time = 0
+						ps.Publish_struct(types.RetractChannel, rm)
+						fmt.Println("发送消息，有撤回", oe.Echo.SelfId, data.Data.MessageId)
 					}
-					var rm iapi.RetractMessage
-					rm.MessageId = data.Data.MessageId
-					rm.SelfId = oe.Echo.SelfId
-					rm.Time = 0
-					ps.Publish_struct(types.RetractChannel, rm)
-					fmt.Println("发送消息，有撤回", oe.Echo.SelfId, data.Data.MessageId)
 				} else {
 					fmt.Println("发送消息，无撤回", oe.Echo.SelfId)
 

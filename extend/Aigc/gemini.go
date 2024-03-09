4af3000e1661ef78a977954b5c965eaa3447d3a1
tuuz <silverhawk@foxmail.com>
2024-03-09 19:04:38 +0800

    更新redis

---
+++ extend/Aigc/gemini.go
@@ -0,0 +1,29 @@
+package Aigc
+
+import (
+	Net "github.com/tobycroft/TuuzNet"
+	"main.go/app/bot/model/SystemParamModel"
+)
+
+func Aigc_gemini_text(text string) (AigcGeminiStruct, error) {
+	post := Net.Post{}.PostFormData("http://aigc.aerofsx.com/v1/aigc/gemini/text", map[string]interface{}{
+		"token": SystemParamModel.Api_value("aigc"),
+	}, map[string]interface{}{
+		"text": text,
+	}, nil, nil)
+	var ag AigcGeminiStruct
+	err := post.RetJson(&ag)
+	if err != nil {
+		return AigcGeminiStruct{}, err
+	}
+	return ag, err
+}
+
+type AigcGeminiStruct struct {
+	Code int `json:"code"`
+	Data struct {
+		Image []string `json:"image"`
+		Text  string   `json:"text"`
+	} `json:"data"`
+	Echo string `json:"echo"`
+}

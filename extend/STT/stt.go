e3a7a526c717f5399dc8cd1eb7134aa446a0b025
tuuz <silverhawk@foxmail.com>
2024-03-15 23:17:21 +0800

    接入语音能力

---
+++ extend/STT/stt.go
@@ -0,0 +1,36 @@
+package STT
+
+import (
+	"errors"
+	"github.com/tobycroft/Calc"
+	Net "github.com/tobycroft/TuuzNet"
+	"main.go/app/bot/model/SystemParamModel"
+	"time"
+)
+
+func (self *Audio) SpeechToText(file_url string) (string, error) {
+	post := Net.Post{}.SetTimeOut(100*time.Second).PostUrlXEncode("http://10.0.0.182:84/v1/tts/stt/audio", map[string]interface{}{
+		"token": SystemParamModel.Api_value("aigc"),
+	}, map[string]interface{}{
+		"file": file_url,
+	}, map[string]string{}, map[string]string{})
+	audio := Audio{}
+	err := post.RetJson(&audio)
+	if err != nil {
+		return "", err
+	}
+	if audio.Code != 0 {
+		return "", errors.New(Calc.Any2String(audio.Echo))
+	}
+	return audio.Echo, err
+}
+
+func (self Audio) New() *Audio {
+	return &self
+}
+
+type Audio struct {
+	Code int    `json:"code"`
+	Data any    `json:"data"`
+	Echo string `json:"echo"`
+}
